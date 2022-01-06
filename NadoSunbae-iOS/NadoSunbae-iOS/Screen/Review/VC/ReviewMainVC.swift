f5f8c989a9a7549e0e1027ad45c0a1f45cc50bb5
eunjuchoi <eunjuzzoo@naver.com>
2022-01-07 04:05:28 +0900

    ✨Feat: 상단 네비 바 UI 구현 (#5)
    - 드롭 섀도우 효과 추가

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/VC/ReviewMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/VC/ReviewMainVC.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class ReviewMainVC: UIViewController {
 
+    @IBOutlet weak var naviBarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addShadowToNaviBar()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension ReviewMainVC {
+    private func addShadowToNaviBar() {
+        naviBarView.layer.shadowColor = UIColor(red: 0.898, green: 0.898, blue: 0.91, alpha: 0.16).cgColor
+        naviBarView.layer.shadowOffset = CGSize(width: 0, height: 9)
+        naviBarView.layer.shadowRadius = 18
+        naviBarView.layer.shadowOpacity = 1
+        naviBarView.layer.masksToBounds = false
+    }
+}
+
+
