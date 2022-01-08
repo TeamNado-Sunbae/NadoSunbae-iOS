6a14a7d150a45e536ec9572d99b7f891893b90dd
Jungbin Jung <lollin0@naver.com>
2022-01-09 03:33:16 +0900

    ✅ Chore: tapSignInBtn 액션 추가

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignIn/VC/SignInVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignIn/VC/SignInVC.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 1v1 on 2022/01/08.
 //
+// TODO: TextField clear Btn 에셋 나오면 수정
 
 import UIKit
 
@@ -11,13 +12,15 @@ class SignInVC: BaseVC {
     // MARK: Properties
     @IBOutlet weak var emailTextField: NadoTextField!
     @IBOutlet weak var pwTextField: NadoTextField!
+    @IBOutlet weak var signInBtn: NadoSunbaeBtn!
+    @IBOutlet weak var infoLabel: UILabel!
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     private func configureUI() {
-        
+        infoLabel.isHidden = true
     }
     
     // MARK: IBAction
@@ -27,6 +30,12 @@ class SignInVC: BaseVC {
     
     @IBAction func tapPWClearBtn(_ sender: Any) {
         pwTextField.text = ""
+    }
+    
+    @IBAction func tapSignInBtn(_ sender: Any) {
+        let sb = NadoSunbaeTBC()
+        sb.modalPresentationStyle = .fullScreen
+        self.present(sb, animated: true, completion: nil)
     }
     
     @IBAction func tapSignUpBtn(_ sender: Any) {
