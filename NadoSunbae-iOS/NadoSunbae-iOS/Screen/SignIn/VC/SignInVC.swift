3614f7f4f514ba7750ac79214ffd091c0bb75844
Jungbin Jung <lollin0@naver.com>
2022-01-08 15:02:36 +0900

    ➕ Add: SignInVC

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignIn/VC/SignInVC.swift
@@ -0,0 +1,14 @@
+//
+//  SignInVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/08.
+//
+
+import UIKit
+
+class SignInVC: BaseVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
