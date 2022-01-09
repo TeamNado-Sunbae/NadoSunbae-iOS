512f1732b2dd9faa117d5574d2f81718095f3abe
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-10 02:03:21 +0900

    ➕ Add: 질문 상세보기뷰 내 메시지UI를 위한 CVC 파일 생성(Question/Comment) (#15)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomQuestionCVC.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomQuestionCVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/10.
+//
+
+import UIKit
+
+class ClassroomQuestionCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
