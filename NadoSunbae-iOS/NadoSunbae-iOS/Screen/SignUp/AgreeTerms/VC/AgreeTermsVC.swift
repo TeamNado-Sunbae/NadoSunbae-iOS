fa2e3c60868da87b47483f14fa5f874993e89734
Jungbin Jung <lollin0@naver.com>
2022-01-09 18:51:26 +0900

    ➕ Add: AgreeTermsVC Class, SB 생성

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/AgreeTermsVC.swift
@@ -0,0 +1,26 @@
+//
+//  AgreeTermsVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/09.
+//
+
+import UIKit
+
+class AgreeTermsVC: BaseVC {
+    // MARK: Properties
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Custom Method
+    private func configureUI() {
+        
+    }
+    
+    // MARK: IBAction
+    
+
+}
