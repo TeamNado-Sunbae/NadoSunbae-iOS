ad0b08282c2f5b6d584cdb428df161d87ee02691
Jungbin Jung <lollin0@naver.com>
2022-01-09 20:36:09 +0900

    ✨ Feat: 약관 동의 체크박스 구현(#17)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/AgreeTermsVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/AgreeTermsVC.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class AgreeTermsVC: BaseVC {
     
     // MARK: Properties
     @IBOutlet weak var allCheckView: UIView!
     @IBOutlet weak var allCheckBtn: UIButton!
+    @IBOutlet weak var checkPrivacyBtn: UIButton!
+    @IBOutlet weak var checkServiceTermBtn: UIButton!
+    
+    let disposeBag = DisposeBag()
     
     // MARK: LifeCycle
     override func viewDidLoad() {
@@ -20,18 +26,38 @@ class AgreeTermsVC: BaseVC {
     }
     
     // MARK: Custom Method
-//    @IBDesignable
     private func configureUI() {
         allCheckView.layer.borderColor = UIColor.gray0.cgColor
         allCheckView.layer.borderWidth = 1
         allCheckView.makeRounded(cornerRadius: 8.adjusted)
-        allCheckBtn.setImgByName(name: "btn_check", selectedName: "btn_check_selected")
+        [allCheckBtn, checkPrivacyBtn, checkServiceTermBtn].forEach { btn in
+            btn.setImgByName(name: "btn_check", selectedName: "btn_check_selected")
+        }
+    }
+    
+    private func changeAllBtnState() {
+        if checkPrivacyBtn.isSelected == checkServiceTermBtn.isSelected {
+            allCheckBtn.isSelected = checkPrivacyBtn.isSelected
+        } else {
+            allCheckBtn.isSelected = false
+        }
     }
     
     // MARK: IBAction
     @IBAction func tapAllCheckBtn(_ sender: UIButton) {
         sender.isSelected.toggle()
+        [checkPrivacyBtn, checkServiceTermBtn].forEach { btn in
+            btn?.isSelected = sender.isSelected
+        }
     }
     
-
+    @IBAction func tapCheckPrivacyBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        changeAllBtnState()
+    }
+    
+    @IBAction func tapCheckServiceTermBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        changeAllBtnState()
+    }
 }
