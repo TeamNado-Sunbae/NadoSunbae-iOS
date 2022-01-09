30853b0ef7e04ca38c28d17c55be01f539aa87e9
Jungbin Jung <lollin0@naver.com>
2022-01-09 23:54:43 +0900

    ➕ Add: SignUpMajorInfoVC

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUpMajorInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpMajorInfoVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/09.
+//
+
+import UIKit
+
+class SignUpMajorInfo: BaseVC {
+    
+    // MARK: Properties
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: Custom Method
+    private func configureUI() {
+
+    }
+    
+    // MARK: IBAction
+}
