7dde7e5a6e7f6fd0baac248be4c6541535eb5144
Jungbin Jung <lollin0@naver.com>
2022-01-09 19:26:30 +0900

    ✅ Chore: 전체 동의 영역 및 체크박스 구현

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/AgreeTermsVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/AgreeTermsVC.swift
@@ -8,19 +8,30 @@
 import UIKit
 
 class AgreeTermsVC: BaseVC {
+    
     // MARK: Properties
+    @IBOutlet weak var allCheckView: UIView!
+    @IBOutlet weak var allCheckBtn: UIButton!
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
     }
     
     // MARK: Custom Method
+//    @IBDesignable
     private func configureUI() {
-        
+        allCheckView.layer.borderColor = UIColor.gray0.cgColor
+        allCheckView.layer.borderWidth = 1
+        allCheckView.makeRounded(cornerRadius: 8.adjusted)
+        allCheckBtn.setImgByName(name: "btn_check", selectedName: "btn_check_selected")
     }
     
     // MARK: IBAction
+    @IBAction func tapAllCheckBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
+    }
     
 
 }
