e62e5aafc5772501a8e3456771f5616e743ddb64
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-10 14:51:13 +0900

    ➕ Add: 질문 상세보기뷰 내 메시지UI를 위한 TVC 파일 생성(Question/Comment) 추가 (CVC 파일 삭제) (#15)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomCommentTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ClassroomCommentTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/10.
+//
+
+import UIKit
+
+class ClassroomCommentTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
