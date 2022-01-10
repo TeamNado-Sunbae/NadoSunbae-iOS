e91eeb607e29ce119c37d5b6658e3cd47b5625a0
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-10 16:53:20 +0900

    ➕ Add: Protocol, VC 추가 (#15)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/UITableViewCellDynamicUpdate.swift
@@ -0,0 +1,8 @@
+//
+//  UITableViewCellDynamicUpdate.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/10.
+//
+
+import Foundation
---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/DefaultQuestionChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultQuestionChatVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/10.
+//
+
+import UIKit
+
+class DefaultQuestionChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
