1e229618b3a4987de132ce115d03c620467adf37
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-12 02:43:39 +0900

    ✨ Feat: 1:1 채팅 목록 조회 VC 구현 완료 (#15)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/DefaultQuestionChatVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/DefaultQuestionChatVC.swift
@@ -8,22 +8,72 @@
 import UIKit
 
 class DefaultQuestionChatVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: IBOutlet
+    @IBOutlet var defaultQuestionChatTV: UITableView! {
+        didSet {
+            defaultQuestionChatTV.dataSource = self
+            defaultQuestionChatTV.allowsSelection = false
+            defaultQuestionChatTV.separatorStyle = .none
+            defaultQuestionChatTV.rowHeight  = UITableView.automaticDimension
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerXib()
     }
-    */
+    
+    // MARK: Custom Methods
+    /// TableView에 Xib 등록하는 메서드
+    private func registerXib() {
+        ClassroomQuestionTVC.register(target: defaultQuestionChatTV)
+        ClassroomCommentTVC.register(target: defaultQuestionChatTV)
+        ClassroomQuestionEditTVC.register(target: defaultQuestionChatTV)
+        ClassroomCommentEditTVC.register(target: defaultQuestionChatTV)
+    }
+}
 
+// MARK: - UITableViewDataSource
+extension DefaultQuestionChatVC: UITableViewDataSource {
+    
+    /// numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return defaultQuestionData.count
+    }
+    
+    /// cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let questionCell = tableView.dequeueReusableCell(withIdentifier: ClassroomQuestionTVC.className) as? ClassroomQuestionTVC,
+              let commentCell = tableView.dequeueReusableCell(withIdentifier: ClassroomCommentTVC.className) as? ClassroomCommentTVC else { return UITableViewCell() }
+        
+        if defaultQuestionData[indexPath.row].isWriter == true {
+            questionCell.dynamicUpdateDelegate = self
+            questionCell.bind(defaultQuestionData[indexPath.row])
+            return questionCell
+        } else {
+            commentCell.dynamicUpdateDelegate = self
+            commentCell.bind(defaultQuestionData[indexPath.row])
+            return commentCell
+        }
+    }
+}
+
+// MARK: - UITableViewCellDynamicUpdate
+extension DefaultQuestionChatVC: TVCHeightDynamicUpdate {
+    
+    /// textView의 높이를 동적으로 업데이트하는 메서드
+    func updateTextViewHeight(cell: UITableViewCell, textView: UITextView) {
+        let size = textView.bounds.size
+        let newSize = defaultQuestionChatTV.sizeThatFits(CGSize(width: size.width,
+                                                                height: CGFloat.greatestFiniteMagnitude))
+        
+        if size.height != newSize.height {
+            UIView.setAnimationsEnabled(false)
+            defaultQuestionChatTV.beginUpdates()
+            defaultQuestionChatTV.endUpdates()
+            UIView.setAnimationsEnabled(true)        }
+    }
 }
