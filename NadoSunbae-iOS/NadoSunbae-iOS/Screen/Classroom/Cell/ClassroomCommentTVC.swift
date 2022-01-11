83180c0050e42fc922ff8782fa7ebee9bc0e75d0
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-12 02:42:42 +0900

    ✅ Chore: 채팅셀 TVC 동적 높이 업데이트까지 구현 완료 (질문/댓글 모두) (#15)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomCommentTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomCommentTVC.swift
@@ -7,17 +7,71 @@
 
 import UIKit
 
-class ClassroomCommentTVC: UITableViewCell {
-
+class ClassroomCommentTVC: BaseTVC {
+    
+    // MARK: IBOutlet
+    @IBOutlet var backView: UIView!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var majorLabel: UILabel!
+    @IBOutlet var commentContentTextView: UITextView!
+    
+    // MARK: Properties
+    weak var dynamicUpdateDelegate: TVCHeightDynamicUpdate?
+    
+    // MARK: LifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configurequestionContentTextView()
+        configureBackView()
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: IBAction
+    @IBAction func tapMoreBtn(_ sender: UIButton) {
+    }
+}
+
+// MARK: - UI
+extension ClassroomCommentTVC {
+    
+    /// questionContentTextView style 구성하는 메서드
+    func configurequestionContentTextView() {
+        commentContentTextView.delegate = self
+        commentContentTextView.isScrollEnabled = false
+        commentContentTextView.isEditable = false
+        commentContentTextView.sizeToFit()
+    }
+    
+    /// backView style 구성하는 메서드
+    func configureBackView() {
+        backView.layer.borderWidth = 1
+        backView.layer.borderColor = UIColor.gray1.cgColor
+        backView.layer.cornerRadius = 8
+    }
+}
+
+// MARK: - Custom Methods
+extension ClassroomCommentTVC {
+    
+    /// 데이터 바인딩하는 메서드
+    func bind(_ model: DefaultQuestionDataModel) {
+        nicknameLabel.text = model.nickname
+        majorLabel.text = model.majorInfo
+        commentContentTextView.text = model.contentText
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension ClassroomCommentTVC: UITextViewDelegate {
+    
+    /// textView가 변화할 때마다 호출되는 메서드
+    func textViewDidChange(_ textView: UITextView) {
+        
+        if let delegate = dynamicUpdateDelegate {
+            delegate.updateTextViewHeight(cell: self, textView: textView)
+        }
+    }
 }
--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomQuestionTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomQuestionTVC.swift
@@ -7,17 +7,73 @@
 
 import UIKit
 
-class ClassroomQuestionTVC: UITableViewCell {
-
+class ClassroomQuestionTVC: BaseTVC {
+    
+    // MARK: IBOutlet
+    @IBOutlet var backView: UIView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var majorLabel: UILabel!
+    @IBOutlet var questionContentTextView: UITextView!
+    
+    // MARK: Properties
+    weak var dynamicUpdateDelegate: TVCHeightDynamicUpdate?
+    
+    // MARK: LifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureQuestionContentTextView()
+        configureBackView()
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
+// MARK: - UI
+extension ClassroomQuestionTVC {
+    
+    /// questionContentTextView style 구성하는 메서드
+    func configureQuestionContentTextView() {
+        questionContentTextView.delegate = self
+        questionContentTextView.isScrollEnabled = false
+        questionContentTextView.isEditable = false
+        questionContentTextView.sizeToFit()
+    }
+    
+    /// backView style 구성하는 메서드
+    func configureBackView() {
+        backView.layer.borderWidth = 1
+        backView.layer.borderColor = UIColor.gray1.cgColor
+        backView.layer.cornerRadius = 8
+    }
+    
+    /// titleLabel style 구성하는 메서드
+    func configureTitleLabel() {
+        titleLabel.sizeToFit()
+    }
+}
+
+// MARK: - Custom Methods
+extension ClassroomQuestionTVC {
+    
+    /// 데이터 바인딩하는 메서드
+    func bind(_ model: DefaultQuestionDataModel) {
+        titleLabel.text = model.questionTitle
+        nicknameLabel.text = model.nickname
+        majorLabel.text = model.majorInfo
+        questionContentTextView.text = model.contentText
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension ClassroomQuestionTVC: UITextViewDelegate {
+    
+    /// textView가 변화할 때마다 호출되는 메서드
+    func textViewDidChange(_ textView: UITextView) {
+        if let delegate = dynamicUpdateDelegate {
+            delegate.updateTextViewHeight(cell: self, textView: textView)
+        }
+    }
 }
