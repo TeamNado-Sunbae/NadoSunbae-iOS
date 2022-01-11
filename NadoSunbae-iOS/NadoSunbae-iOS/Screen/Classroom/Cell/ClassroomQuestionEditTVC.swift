40e8dbc1233e563754ab3130b5ad871510ea2520
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-12 02:21:40 +0900

    ➕ Add: 메시지 수정을 위한 Edit Question/Comment 파일 생성 (Xib, TVC) (#15)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/ClassroomQuestionEditTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ClassroomQuestionEditTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/11.
+//
+
+import UIKit
+
+class ClassroomQuestionEditTVC: BaseTVC {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
