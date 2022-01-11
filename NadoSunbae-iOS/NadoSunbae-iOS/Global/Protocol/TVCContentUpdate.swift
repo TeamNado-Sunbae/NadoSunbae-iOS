1abbd11507e3010effb74b4887cdfc1bb4db44e5
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-12 02:44:42 +0900

    ➕ Add: TVCContentUpdate 프로토콜 생성 (#15)
    1:1 질문 채팅셀 수정 클릭 시 cell을 갈아끼우기 위한 TVCContentUpdate을 생성

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/TVCContentUpdate.swift
@@ -0,0 +1,13 @@
+//
+//  TVCContentUpdate.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/11.
+//
+
+import UIKit
+
+protocol TVCContentUpdate : AnyObject {
+    
+    func updateTV()
+}
