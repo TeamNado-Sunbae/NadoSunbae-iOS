1de9af24983708c4dfa53f2141541b3a0ae60c33
Jungbin Jung <lollin0@naver.com>
2022-01-12 02:38:05 +0900

    ✅ Chore: 완료 UI 구성 (#25)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/SignUpComplete/VC/SignUpCompleteVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpCompleteVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/12.
+//
+
+import UIKit
+
+class SignUpCompleteVC: BaseVC {
+    
+    // MARK: Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterBtn: NadoSunbaeBtn!
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: Custom Method
+    private func configureUI() {
+    }
+    
+    // MARK: IBAction
+    
+
+}
