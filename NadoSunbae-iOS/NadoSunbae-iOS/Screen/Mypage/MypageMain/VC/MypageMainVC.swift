9b40bb9de4f41dc64f5178403e3940f00e1619c0
Jungbin Jung <lollin0@naver.com>
2022-01-14 01:12:56 +0900

    ✅ Chore: Embed MypageNVC 및 테스트용 화면 연결

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageMain/VC/MypageMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageMain/VC/MypageMainVC.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class MypageMainVC: UIViewController {
 
+    // MARK: @IBOutlet
+
+    // MARK: Properties
+
+    // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: @IBAction
+    @IBAction func goMypageUser(_ sender: UIButton) {
+        guard let vc = UIStoryboard.init(name: MypageUserVC.className, bundle: nil).instantiateViewController(withIdentifier: MypageUserVC.className) as? MypageUserVC else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
