71861cf0bd73064394665e9754661d823633e59b
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-14 06:03:49 +0900

    ✨ Feat: NadoTextView Custom Class 구현 (#40)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoTextView.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoTextView.swift
@@ -6,15 +6,32 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
+/**
+ 나도선배에서 자주 사용되는 Scroll Indicator Custom TextView.
+ - Note:
+ - configureTitleLabel: titleLabel 텍스트 변경
+ - configureRightCustomBtn: 우측 커스텀 버튼 이미지 변경
+ - setUpNaviStyle: NaviState를 지정해 해당 스타일로 네비바를 구성
+ */
 class NadoTextView: UITextView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    func setDefaultStyle(placeholderText: String) {
+        self.layer.cornerRadius = 8
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.gray0.cgColor
+        self.text = placeholderText
+        self.textColor = .gray2
+        self.font = .PretendardR(size: 14.0)
+        self.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 6
+        let attributes = [NSAttributedString.Key.paragraphStyle: style,
+                          NSAttributedString.Key.font: UIFont.PretendardR(size: 14.0),
+                          NSAttributedString.Key.foregroundColor: UIColor.gray2]
+        self.attributedText = NSAttributedString(string: self.text, attributes: attributes)
     }
-    */
-
 }
