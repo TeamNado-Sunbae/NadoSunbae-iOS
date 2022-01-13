3db0e841de51d01eb5b5cc75aa287ed8b15fa071
eunjuchoi <eunjuzzoo@naver.com>
2022-01-13 16:12:01 +0900

    ➕Add : cell delegate 관련 protocol 추가 (#31)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/ClickActionSheetInCell.swift
@@ -0,0 +1,12 @@
+//
+//  ClickActionSheetInCell.swift
+//  NadoSunbae-iOS
+//
+//  Created by EUNJU on 2022/01/13.
+//
+
+import Foundation
+
+protocol ClickActionSheetInCell: AnyObject {
+    func SendActionSheetSelection(index: Int) 
+}
