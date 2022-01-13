d8a1e1fc969343f7847ee2e7d549b4aeb0760221
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-14 06:01:23 +0900

    ➕ Add: 필요한 파일 추가 (#40)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Extension/UIScrollView+.swift
@@ -0,0 +1,8 @@
+//
+//  UIScrollView+.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/14.
+//
+
+import Foundation
---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoTextView.swift
@@ -0,0 +1,20 @@
+//
+//  NadoTextView.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/14.
+//
+
+import UIKit
+
+class NadoTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
