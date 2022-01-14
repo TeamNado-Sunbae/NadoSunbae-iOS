e09594a7c840fe06094a6b20a3a663c173700675
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-14 15:18:40 +0900

    ➕ Add: WriteQuestion 파일 추가 (#44)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/WriteQuestionVC.swift
@@ -0,0 +1,29 @@
+//
+//  WriteQuestionVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by 황지은 on 2022/01/14.
+//
+
+import UIKit
+
+class WriteQuestionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
