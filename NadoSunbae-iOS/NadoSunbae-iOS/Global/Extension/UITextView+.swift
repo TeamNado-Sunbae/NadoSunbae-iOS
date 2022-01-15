6b51ec2e1f32ea2271d43a9407e9e3051ae48633
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-15 17:20:12 +0900

    ✅ Chore: UITextView+ 라인 수 리턴하는 메서드 추가 (#44)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/Extension/UITextView+.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Extension/UITextView+.swift
@@ -24,4 +24,20 @@ extension UITextView {
         let positiveTopOffset = max(1, topOffset)
         contentOffset.y = -positiveTopOffset
     }
+    
+    /// line 수 카운트 메서드
+    func numberOfLines() -> Int {
+        let layoutManager = layoutManager
+        let numberOfGlyphs = layoutManager.numberOfGlyphs
+        var lineRange: NSRange = NSMakeRange(0, 1)
+        var index = 0
+        var numberOfLines = 0
+
+        while index < numberOfGlyphs {
+            layoutManager.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange)
+            index = NSMaxRange(lineRange)
+            numberOfLines += 1
+        }
+        return numberOfLines
+    }
 }
