03067af7133222477fe6a44d3fb459a212a24179
Jungbin Jung <lollin0@naver.com>
2022-01-16 17:36:23 +0900

    ✅ Chore: Btn IBAction 추가 (#49)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageMain/VC/MypageMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageMain/VC/MypageMainVC.swift
@@ -39,6 +39,34 @@ class MypageMainVC: UIViewController {
         guard let vc = UIStoryboard.init(name: MypageUserVC.className, bundle: nil).instantiateViewController(withIdentifier: MypageUserVC.className) as? MypageUserVC else { return }
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+    @IBAction func tapEditProfileBtn(_ sender: Any) {
+        /// 1순위!
+        self.navigationController?.pushViewController(UIViewController(), animated: true)
+    }
+    
+    @IBAction func tapSettingBtn(_ sender: Any) {
+        /// 4순위댱
+    }
+    
+    @IBAction func tapLikeListBtn(_ sender: Any) {
+        /// 4순위
+    }
+    
+    @IBAction func tapSortBtn(_ sender: Any) {
+        /// 3-4순위`
+        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let latestAction = UIAlertAction(title: "최신순", style: .default, handler: nil)
+        let moreLikeAction = UIAlertAction(title: "좋아요순", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        [latestAction, moreLikeAction, cancelAction].forEach { action in
+            optionMenu.addAction(action)
+        }
+        
+        self.present(optionMenu, animated: true, completion: nil)
+    }
 }
 
 // MARK: - UI
