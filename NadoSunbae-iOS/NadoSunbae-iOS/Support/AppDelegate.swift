0752bf96fe540b79d28b0914ec06713335542433
Jungbin Jung <lollin0@naver.com>
2022-01-16 21:30:21 +0900

    ➕ Add: pod 설치 및 초기화 코드 추가 (#54)

--- NadoSunbae-iOS/NadoSunbae-iOS/Support/AppDelegate.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Support/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        /// Firebase 초기화
+        FirebaseApp.configure()
         return true
     }
     
