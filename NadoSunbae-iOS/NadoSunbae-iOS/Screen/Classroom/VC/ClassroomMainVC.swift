34df9c40d3ef287c6de94b6084d7ad5736f51b45
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-17 00:37:34 +0900

    ✅ Chore: ContainerView에 질문, 정보 VC 연결 (#47)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
@@ -12,6 +12,7 @@ import Then
 class ClassroomMainVC: UIViewController {
     
     // MARK: Properties
+    private let screenWidth = UIScreen.main.bounds.size.width
     private let majorLabel = UILabel().then {
         // TODO: Userdefaults선배 값으로 바꿀 예정
         $0.text = "국어국문학과"
@@ -26,11 +27,16 @@ class ClassroomMainVC: UIViewController {
     
     private let majorSelectBtn = UIButton()
     
+    // MARK: IBOutlet
     @IBOutlet var topNaviView: UIView!
+    private let classroomContainerView = NadoHorizonContainerViews()
+    
+    // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         tapMajorSelectBtn()
+        configureContainerView()
     }
 }
 
@@ -38,6 +44,7 @@ class ClassroomMainVC: UIViewController {
 extension ClassroomMainVC {
     private func configureUI() {
         topNaviView.addSubviews([majorLabel, bottomArrowImgView, majorSelectBtn])
+        self.view.addSubview(classroomContainerView)
         
         majorLabel.snp.makeConstraints {
             $0.bottom.equalTo(topNaviView.snp.bottom).offset(-24)
@@ -54,6 +61,41 @@ extension ClassroomMainVC {
             $0.top.bottom.trailing.equalTo(bottomArrowImgView)
             $0.leading.equalTo(majorLabel)
         }
+        
+        classroomContainerView.snp.makeConstraints {
+            $0.top.equalTo(topNaviView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    /// containerView 컨텐츠 VC를 설정하는 메서드
+    func configureContainerView() {
+        let questionSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionSB, bundle: nil)
+        let infoSB: UIStoryboard = UIStoryboard(name: Identifiers.InfoSB, bundle: nil)
+
+        guard let questionMainVC = questionSB.instantiateViewController(identifier: QuestionMainVC.className) as? QuestionMainVC else { return }
+        guard let infoMainVC = infoSB.instantiateViewController(identifier: InfoMainVC.className) as? InfoMainVC else { return }
+        
+        questionMainVC.sendSegmentStateDelegate = self
+        infoMainVC.sendSegmentStateDelegate = self
+        
+        [questionMainVC, infoMainVC].forEach {
+            addChild($0)
+            $0.view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        classroomContainerView.firstContainerView.addSubview(questionMainVC.view)
+        classroomContainerView.secondContainerView.addSubview(infoMainVC.view)
+        
+        questionMainVC.view.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(self.classroomContainerView)
+        }
+        
+        infoMainVC.view.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(classroomContainerView.secondContainerView)
+        }
+        
+        questionMainVC.didMove(toParent: self)
     }
 }
 
@@ -78,7 +120,19 @@ extension ClassroomMainVC {
 
 // MARK: - UIViewControllerTransitioningDelegate
 extension ClassroomMainVC: UIViewControllerTransitioningDelegate {
+    
+    /// presentationController - forPresented
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         HalfModalPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+extension ClassroomMainVC: SendSegmentStateDelegate {
+    func sendSegmentClicked(index: Int) {
+        if index == 0 {
+            classroomContainerView.externalSV.contentOffset.x = 0
+        } else {
+            classroomContainerView.externalSV.contentOffset.x += screenWidth
+        }
     }
 }
