0e525d3add7fa584f50337f78cc18677773d3fd3
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-17 18:29:50 +0900

    ✅ Chore: 과방탭 RootVC를 ClassroomNC로 지정 (#47)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomNC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomNC.swift
@@ -7,23 +7,12 @@
 
 import UIKit
 
-class ClassroomNC: UINavigationController {
+class ClassroomNC: BaseNC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let classroomMainVC = self.storyboard?.instantiateViewController(withIdentifier: ClassroomMainVC.className) as? ClassroomMainVC else { return }
+        self.setViewControllers([classroomMainVC], animated: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
