883769f9b4dfe3abbb31f3d23189c5db6980b7f5
Jungbin Jung <lollin0@naver.com>
2022-01-18 01:26:05 +0900

    ✅ Chore: extension으로 코드 정리 및 주석 추가

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Notification/Cell/NotificationTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Notification/Cell/NotificationTVC.swift
@@ -34,28 +34,34 @@ class NotificationTVC: BaseTVC {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
-    // MARK: Custom Methods
+}
+
+// MARK: - UI
+extension NotificationTVC {
     private func configureUI() {
         self.makeRounded(cornerRadius: 8.adjusted)
         self.backgroundColor = .clear
     }
+}
     
+// MARK: - Custom Methods
+extension NotificationTVC {
     func setData(data: NotificationModel) {
         redCircleImgView.isHidden = !(data.isRead)
         titleLabel.text = "\(data.notiType.rawValue)에 \(data.senderNickName)님이 \(data.notiType != .mypageQuestion ? "답글" : "1:1 질문")을 남겼습니다."
         
+        /// 알림 case별 텍스트 컬러 변경을 위한 Attribute 사용
         let mintAttributeStr = NSMutableAttributedString(string: titleLabel.text!)
 
         switch data.notiType {
         case .mypageQuestion:
-            mintAttributeStr.addAttributes([.foregroundColor : UIColor.blue], range: (titleLabel.text! as NSString).range(of: "1:1 질문"))
+            mintAttributeStr.addAttributes([.foregroundColor : UIColor.mainDefault], range: (titleLabel.text! as NSString).range(of: "1:1 질문"))
         default:
-            mintAttributeStr.addAttributes([.foregroundColor : UIColor.blue], range: (titleLabel.text! as NSString).range(of: "\(data.notiType.rawValue)"))
+            mintAttributeStr.addAttributes([.foregroundColor : UIColor.mainDefault], range: (titleLabel.text! as NSString).range(of: "\(data.notiType.rawValue)"))
         }
         
         titleLabel.attributedText = mintAttributeStr
-        
+
         contentLabel.text = "\(data.content)"
         timeLabel.text = "\(data.time)"
     }
