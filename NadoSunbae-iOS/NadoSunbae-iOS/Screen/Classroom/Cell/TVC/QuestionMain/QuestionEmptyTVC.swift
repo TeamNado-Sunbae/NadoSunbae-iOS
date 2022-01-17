6e8e1d1076f0a80fe5eb39e41b4bfb0ad8b371f8
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-17 16:32:14 +0900

    ➕ Add: QuestionEmptyTVC 추가 (#47)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/QuestionMain/QuestionEmptyTVC.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionEmptyTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/01/17.
+//
+
+import UIKit
+
+class QuestionEmptyTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
