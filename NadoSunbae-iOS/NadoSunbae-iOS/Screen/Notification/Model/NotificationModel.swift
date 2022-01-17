0dfe9770401aa60f681f82e46cc9bc1c777065a7
Jungbin Jung <lollin0@naver.com>
2022-01-17 11:30:00 +0900

    ✅ Chore: Notification Model 작성 (#59)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Notification/Model/NotificationModel.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Notification/Model/NotificationModel.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
-struct NotificationModel: Codable {
+enum NotiType: Codable {
     
+    /// 작성하신 질문글
+    case writtenQuestion
+    
+    /// 작성하신 정보글
+    case writtenInfo
+    
+    /// 답글을 작성하신 질문글
+    case answerQuestion
+    
+    /// 답글을 작성하신 정보글
+    case answerInfo
+    
+    /// 알림을 켜둔 질문글
+    case notiQuestion
+    
+    /// 알림을 켜둔 정보글
+    case notiInfo
+    
+    /// 마이페이지에 온 1:1 질문
+    case mypageQuestion
+}
+
+struct NotificationModel: Codable {
+    var senderNickName: String
+    var notiType: NotiType
+    var time: String
+    var isRead: Bool
+    var title: String
+    var content: String
 }
