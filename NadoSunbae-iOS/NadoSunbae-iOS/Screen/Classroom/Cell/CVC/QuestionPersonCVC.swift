8b81ecbdc4f7bad0e8bb6df85d3f1c7681708fdd
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-18 04:47:32 +0900

    ✅ Chore: 질문가능선배 CVC 구현 (#61)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/CVC/QuestionPersonCVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/CVC/QuestionPersonCVC.swift
@@ -6,7 +6,93 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class QuestionPersonCVC: UICollectionViewCell {
+class QuestionPersonCVC: CodeBaseCVC {
     
+    // MARK: Properties
+    private let personProfileImageView = UIImageView().then {
+        $0.image = UIImage(named: "imgProfile")
+    }
+    
+    private let nicknameLabel = UILabel().then {
+        $0.textColor = .nadoBlack
+        $0.font = .PretendardSB(size: 14.0)
+        $0.sizeToFit()
+    }
+    
+    private let majorNameStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.alignment = .fill
+        $0.spacing = 4
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.distribution = .fill
+    }
+    
+    private let majorLabel = UILabel().then {
+        $0.textColor = .mainDefault
+        $0.font = .PretendardR(size: 12.0)
+        $0.sizeToFit()
+    }
+    
+    private let majorStartLabel = UILabel().then {
+        $0.textColor = .gray4
+        $0.font = .PretendardR(size: 12.0)
+        $0.sizeToFit()
+    }
+    
+    override func setupViews() {
+        configureUI()
+    }
+}
+
+// MARK: - UI
+extension QuestionPersonCVC {
+    
+    /// UI구성 메서드
+    private func configureUI() {
+        self.addSubviews([personProfileImageView, nicknameLabel, majorNameStackView])
+        majorNameStackView.addArrangedSubview(majorLabel)
+        majorNameStackView.addArrangedSubview(majorStartLabel)
+        
+        personProfileImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(4)
+            $0.height.width.equalTo(72)
+            $0.centerX.equalTo(contentView)
+        }
+        
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(personProfileImageView.snp.bottom).offset(8)
+            $0.centerX.equalTo(personProfileImageView)
+        }
+        
+        majorNameStackView.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(4)
+            $0.centerX.equalTo(personProfileImageView)
+        }
+    }
+}
+
+// MARK: - Custom Methods
+extension QuestionPersonCVC {
+    func onSetData(model: OnQuestionUserListModel) {
+        nicknameLabel.text = model.nickname
+        if model.isFirstMajor == true {
+            majorLabel.text = "본"
+        } else {
+            majorLabel.text = "제2"
+        }
+        majorStartLabel.text = model.majorStart + "진입"
+    }
+    
+    func offSetData(model: OffQuestionUserListModel) {
+        nicknameLabel.text = model.nickname
+        if model.isFirstMajor == true {
+            majorLabel.text = "본"
+        } else {
+            majorLabel.text = "제2"
+        }
+        majorStartLabel.text = model.majorStart + "진입"
+    }
 }
