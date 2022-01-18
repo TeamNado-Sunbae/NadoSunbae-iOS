366bcbdc069bd29a83883b08f3cd757766f72621
Jungbin Jung <lollin0@naver.com>
2022-01-19 00:49:28 +0900

    ✅ Chore: 마이/선배페이지 - 1:1 질문 페이지 연결 (#77)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageUser/VC/MypageUserVC+TV.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageUser/VC/MypageUserVC+TV.swift
@@ -27,8 +27,13 @@ extension MypageUserVC: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: 나중에 1:1 뷰랑 연결
-        let vc = UIViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
+        let groupChatSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionChatSB, bundle: nil)
+        guard let groupChatVC = groupChatSB.instantiateViewController(identifier: DefaultQuestionChatVC.className) as? DefaultQuestionChatVC else { return }
+        
+        // TODO: 추후에 Usertype, isWriter 정보도 함께 넘길 예정(?)
+        groupChatVC.questionType = .personal
+        groupChatVC.naviStyle = .push
+        
+        self.navigationController?.pushViewController(groupChatVC, animated: true)
     }
 }
