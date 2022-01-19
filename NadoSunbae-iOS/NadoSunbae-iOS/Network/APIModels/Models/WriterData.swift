1eb6efc371c3cafc89507b01241965fd78f52d74
eunjuchoi <eunjuzzoo@naver.com>
2022-01-19 09:44:48 +0900

    ➕ Add: 서버통신을 위한 모델 생성 (#90)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/WriterData.swift
@@ -0,0 +1,21 @@
+//
+//  WriterData.swift
+//  NadoSunbae-iOS
+//
+//  Created by EUNJU on 2022/01/19.
+//
+
+import Foundation
+
+// MARK: - Writer
+struct Writer: Codable {
+    let writerID: Int
+    let nickname, firstMajorName, firstMajorStart, secondMajorName: String
+    let secondMajorStart: String
+    let isOnQuestion, isReviewed: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case writerID = "writerId"
+        case nickname, firstMajorName, firstMajorStart, secondMajorName, secondMajorStart, isOnQuestion, isReviewed
+    }
+}
