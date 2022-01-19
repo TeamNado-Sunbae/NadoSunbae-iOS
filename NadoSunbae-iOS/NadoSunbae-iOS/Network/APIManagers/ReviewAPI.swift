0ec3c6962ce50b5821cc84ec760d7a70b3c08636
eunjuchoi <eunjuzzoo@naver.com>
2022-01-20 06:16:43 +0900

    ✅ Chore: Service Taget,  API 하나의 파일로 합침 (#96)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/ReviewAPI.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/ReviewAPI.swift
@@ -30,6 +30,23 @@ class ReviewAPI {
             }
         }
     }
+    
+    /// [POST] 후기글 리스트 API
+    func getReviewPostListAPI(majorID: Int, writerFilter: Int, tagFilter: [Int], completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        userProvider.request(.getReviewMainPostList(majorID: majorID, writerFilter: writerFilter, tagFilter: tagFilter, sort: "recent")) { result in
+            switch result {
+                
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                completion(self.getReviewPostListJudgeData(status: statusCode, data: data))
+                
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
 }
 
 // MARK: - JudgeData
@@ -39,6 +56,23 @@ extension ReviewAPI {
     func createReviewPostJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         let decodedData = try? decoder.decode(GenericResponse<ReviewPostRegisterData>.self, from: data) 
+        
+        switch status {
+        case 200...204:
+            return .success(decodedData?.data ?? "None-Data")
+        case 400...409:
+            return .requestErr(decodedData?.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    /// getReviewPostListJudgeData
+    func getReviewPostListJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        let decodedData = try? decoder.decode(GenericResponse<ReviewMainPostListData>.self, from: data)
         
         switch status {
         case 200...204:
