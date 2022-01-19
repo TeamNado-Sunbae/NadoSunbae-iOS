0024290c9bd4ba10a65ba64f7722fcd1c6c2062e
Jungbin Jung <lollin0@naver.com>
2022-01-20 01:10:34 +0900

    ✅ Chore: Notification API 작성 (#94)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/NotificationAPI.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/NotificationAPI.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import Moya
+
+class NotificationAPI {
+    static let shared = NotificationAPI()
+    private var provider = MoyaProvider<NotificationService>(plugins: [NetworkLoggerPlugin()])
+    
+    private init() {}
+}
+
+// MARK: - API
+extension NotificationAPI {
+    
+    /// [GET] 전체 알림 리스트 조회
+    func getNotiList(completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        provider.request(.getNotiList) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+
+                completion(self.getNotiListJudgeData(status: statusCode, data: data))
+
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
+
+// MARK: - judgeData
+extension NotificationAPI {
+    private func getNotiListJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(GenericResponse<NotificationDataModel>.self, from: data) else { return .pathErr }
+        
+        switch status {
+        case 200...204:
+            return .success(decodedData.data ?? "None-Data")
+        case 400...409:
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
