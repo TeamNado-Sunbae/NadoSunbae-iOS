8052dbb4b04580cff47320a840316b48cfd0239d
eunjuchoi <eunjuzzoo@naver.com>
2022-01-19 19:50:47 +0900

    ➕ Add: 학과 리스트 조회 API 구현 (#92)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/MoyaTarget/PublicService.swift
@@ -0,0 +1,57 @@
+//
+//  PublicService.swift
+//  NadoSunbae-iOS
+//
+//  Created by EUNJU on 2022/01/19.
+//
+
+import Foundation
+import Moya
+
+enum PublicService {
+    case getMajorList(univID: Int, filter: String)
+}
+
+extension PublicService: TargetType {
+    
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+            
+        case .getMajorList(let univID, _):
+            return "/major/list/\(univID)"
+        }
+    }
+    var method: Moya.Method {
+        switch self {
+            
+        case .getMajorList:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+            
+        case .getMajorList(_, let filter):
+            let body = ["filter" : filter]
+            return .requestParameters(parameters: body, encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        //let accessToken: String = UserDefaults.standard.string(forKey: UserDefaults.Keys.AccessToken) ?? ""
+        
+        switch self {
+            
+        case .getMajorList:
+            return ["Content-Type" : "application/json"]
+        }
+    }
+}
+    
+    
+
