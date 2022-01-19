2c801ef0bb0efa4bae2053bad16661d0ea32a649
Jungbin Jung <lollin0@naver.com>
2022-01-19 23:58:10 +0900

    ➕ Add: service, api 파일 생성

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/NotificationAPI.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationAPI.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/19.
+//
+
+import Foundation
---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/MoyaTarget/NotificationService.swift
@@ -0,0 +1,67 @@
+//
+//  NotificationService.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/19.
+//
+
+import Foundation
+import Moya
+
+enum NotificationService {
+    case getNotiList(
+    case createReviewPost(majorID: Int, bgImgID: Int, oneLineReview: String, prosCons: String, curriculum: String, career: String, recommendLecture: String, nonRecommendLecture: String, tip: String)
+}
+
+extension NotificationService: TargetType {
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getNotiList:
+            return "/review-post"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+            
+        case .createReviewPost:
+            return .post
+        }
+    }
+    
+    var task: Task {
+        switch self {
+            
+        case .createReviewPost(let majorID, let bgImgID, let oneLineReview, let prosCons, let curriculum, let career, let recommendLecture, let nonRecommendLecture, let tip):
+            
+            let body: [String : Any] = [
+                "majorId" : majorID,
+                "backgroundImageId" : bgImgID,
+                "oneLineReview" : oneLineReview,
+                "prosCons" : prosCons,
+                "curriculum" : curriculum,
+                "career" : career,
+                "recommendLecture" : recommendLecture,
+                "nonRecommendLecture" : nonRecommendLecture,
+                "tip" : tip
+            ]
+            
+            return .requestParameters(parameters: body, encoding: URLEncoding.httpBody)
+        }
+    }
+    
+    var headers: [String: String]? {
+        let accessToken: String = UserDefaults.standard.string(forKey: UserDefaults.Keys.AccessToken) ?? ""
+        print(accessToken)
+        
+        switch self {
+            
+        case .createReviewPost:
+            return ["accesstoken" : accessToken]
+        }
+    }
+}
