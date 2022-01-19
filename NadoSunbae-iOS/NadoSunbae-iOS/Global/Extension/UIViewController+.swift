7f73fe330e340ce342035896a5d726a42a88d4a4
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-20 08:03:00 +0900

    ✨ Feat: 나도선배에서 자주 쓰이는 ActionSheet AlertController extension 함수로 구현 (#88)
    style은 선택 옵션 2개 + 취소 옵션 / 선택 옵션 1개 + 취소옵션

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/Extension/UIViewController+.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Extension/UIViewController+.swift
@@ -43,43 +43,48 @@ extension UIViewController {
     /**
      - Description: Alert
      */
-    /// 확인, 취소 버튼 Alert 메서드
-    func makeAlertWithCancel(title : String, message : String,
-                             okTitle: String = "확인", okStyle: UIAlertAction.Style = .default,
-                             cancelTitle: String = "취소",
-                             okAction : ((UIAlertAction) -> Void)?, cancelAction : ((UIAlertAction) -> Void)? = nil,
-                             completion : (() -> Void)? = nil) {
+    /// 확인 버튼 2개, 취소 버튼 1개 ActionSheet 메서드
+    func makeTwoAlertWithCancel(okTitle: String, okStyle: UIAlertAction.Style = .default,
+                                secondOkTitle: String, secondOkStyle: UIAlertAction.Style = .default,
+                                cancelTitle: String = "취소",
+                                okAction : ((UIAlertAction) -> Void)?, secondOkAction : ((UIAlertAction) -> Void)?, cancelAction : ((UIAlertAction) -> Void)? = nil,
+                                completion : (() -> Void)? = nil) {
         
         let generator = UIImpactFeedbackGenerator(style: .medium)
         generator.impactOccurred()
         
-        let alertViewController = UIAlertController(title: title, message: message,
-                                                    preferredStyle: .alert)
+        let alertViewController = UIAlertController(title: nil, message: nil,
+                                                    preferredStyle: .actionSheet)
         
         let okAction = UIAlertAction(title: okTitle, style: okStyle, handler: okAction)
         alertViewController.addAction(okAction)
         
+        let secondOkAction = UIAlertAction(title: secondOkTitle, style: secondOkStyle, handler: secondOkAction)
+        alertViewController.addAction(secondOkAction)
         
         let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelAction)
         alertViewController.addAction(cancelAction)
         
-        
         self.present(alertViewController, animated: true, completion: completion)
     }
     
-    /// 확인 버튼 Alert 메서드
-    func makeAlert(title : String, message : String? = nil,
-                   okTitle: String = "확인", okAction : ((UIAlertAction) -> Void)? = nil,
-                   completion : (() -> Void)? = nil) {
+    /// 확인 버튼 1개, 취소 버튼 1개 Alert 메서드
+    func makeAlertWithCancel(okTitle: String, okStyle: UIAlertAction.Style = .default,
+                                cancelTitle: String = "취소",
+                                okAction : ((UIAlertAction) -> Void)?, cancelAction : ((UIAlertAction) -> Void)? = nil,
+                                completion : (() -> Void)? = nil) {
+        
         let generator = UIImpactFeedbackGenerator(style: .medium)
         generator.impactOccurred()
         
-        let alertViewController = UIAlertController(title: title, message: message,
-                                                    preferredStyle: .alert)
+        let alertViewController = UIAlertController(title: nil, message: nil,
+                                                    preferredStyle: .actionSheet)
         
-        let okAction = UIAlertAction(title: okTitle, style: .default, handler: okAction)
+        let okAction = UIAlertAction(title: okTitle, style: okStyle, handler: okAction)
         alertViewController.addAction(okAction)
         
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
         
         self.present(alertViewController, animated: true, completion: completion)
     }
