305690448d945c40342c11dd7933c9405858fe18
hwangJi-dev <melon2228@swu.ac.kr>
2022-01-20 08:05:43 +0900

    ✅ Chore: 채팅 리스트 조회시 response data Model 생성 (#88)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Classroom/QuestionChat/ClassroomQuestionDetailData.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Classroom/QuestionChat/ClassroomQuestionDetailData.swift
@@ -12,27 +12,35 @@ struct ClassroomQuestionDetailData: Codable {
     let questionerID, answererID: Int
     let like: ClassroomQuestionLike
     let messageList: [ClassroomMessageList]
-
+    
     enum CodingKeys: String, CodingKey {
         case questionerID = "questionerId"
         case answererID = "answererId"
         case like, messageList
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        questionerID = (try? values.decode(Int.self, forKey: .questionerID)) ?? -1
+        answererID = (try? values.decode(Int.self, forKey: .answererID)) ?? 0
+        like = (try? values.decode(ClassroomQuestionLike.self, forKey: .like)) ?? ClassroomQuestionLike(isLiked: true, likeCount: "1")
+        messageList = (try? values.decode([ClassroomMessageList].self, forKey: .messageList)) ?? []
     }
 }
 
 // MARK: - ClassroomQuestionLike
 struct ClassroomQuestionLike: Codable {
     let isLiked: Bool
-    let likeCount: Int
+    let likeCount: String
 }
 
-// MARK: - ClassroomQuestionWriter
+// MARK: - ClassroomMessageList
 struct ClassroomMessageList: Codable {
     let messageID: Int
     let title, content, createdAt: String
     let isDeleted: Bool
     let writer: ClassroomQuestionWriter
-
+    
     enum CodingKeys: String, CodingKey {
         case messageID = "messageId"
         case title, content, createdAt, isDeleted, writer
@@ -45,10 +53,18 @@ struct ClassroomQuestionWriter: Codable {
     let isQuestioner: Bool
     let nickname, firstMajorName, firstMajorStart, secondMajorName: String
     let secondMajorStart: String
-
+    
     enum CodingKeys: String, CodingKey {
         case writerID = "writerId"
         case profileImageID = "profileImageId"
         case isQuestioner, nickname, firstMajorName, firstMajorStart, secondMajorName, secondMajorStart
+    }
+}
+
+func convertToMajorInfoString(_ firstMajorName: String, _ firstMajorStart: String, _ secondMajorName: String, _ secondMajorStart: String) -> String {
+    if secondMajorName == "미진입" {
+        return firstMajorName + " " + firstMajorStart + " "
+    } else {
+        return firstMajorName + " " + firstMajorStart + " " + "|" + " " + secondMajorName + " " + secondMajorStart
     }
 }
