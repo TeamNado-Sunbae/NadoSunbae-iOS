e30a940debae0fef04ffe91412f88a0e6dbb2e44
Jungbin Jung <lollin0@naver.com>
2022-01-20 21:52:13 +0900

    ✅ Chore: SignInDataModel 작성 (#98)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Sign/SignInDataModel.swift
@@ -0,0 +1,40 @@
+//
+//  SignInModel.swift
+//  NadoSunbae-iOS
+//
+//  Created by 1v1 on 2022/01/20.
+//
+
+import Foundation
+
+struct SignInDataModel: Codable {
+    var user: User
+    var accesstoken: String
+
+    enum CodingKeys: String, CodingKey {
+        case user = "user"
+        case accesstoken = "accesstoken"
+    }
+    
+    struct User: Codable {
+        var userID: Int
+        var email: String
+        var universityID: Int
+        var firstMajorID: Int
+        var firstMajorName: String
+        var secondMajorID: Int
+        var secondMajorName: String
+        var isReviewed: Bool
+
+        enum CodingKeys: String, CodingKey {
+            case userID = "userId"
+            case email = "email"
+            case universityID = "universityId"
+            case firstMajorID = "firstMajorId"
+            case firstMajorName = "firstMajorName"
+            case secondMajorID = "secondMajorId"
+            case secondMajorName = "secondMajorName"
+            case isReviewed = "isReviewed"
+        }
+    }
+}
