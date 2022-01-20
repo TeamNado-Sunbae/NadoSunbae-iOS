5a22713e9832c5808c6951ec0261b387ad5e28ab
Jungbin Jung <lollin0@naver.com>
2022-01-20 21:52:01 +0900

    ✅ Chore: SignIn API 코드 작성 (#98)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/SignAPI.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIManagers/SignAPI.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import Moya
+
+class SignAPI {
+    static let shared = SignAPI()
+    private var provider = MoyaProvider<SignService>(plugins: [NetworkLoggerPlugin()])
+    
+    private init() {}
+}
+
+// MARK: - API
+extension SignAPI {
+    
+    /// [POST] 로그인 요청
+    func signIn(email: String, PW: String, deviceToken: String, completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        provider.request(.signIn(email: email, PW: PW, deviceToken: deviceToken)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                completion(self.signInJudgeData(status: statusCode, data: data))
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
+
+// MARK: - judgeData
+extension SignAPI {
+    private func signInJudgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(GenericResponse<SignInDataModel>.self, from: data) else { return .pathErr }
+        
+        switch status {
+        case 200...204:
+            return .success(decodedData.data ?? "None-Data")
+        case 400...409:
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
