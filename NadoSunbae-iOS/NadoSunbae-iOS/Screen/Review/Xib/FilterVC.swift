8ab7993a867b2bfc3743c943186e650ba37f1fc0
eunjuchoi <eunjuzzoo@naver.com>
2022-02-05 02:38:09 +0900

    ✨Feat: 필터부분 디폴트 UI 구성 (#157)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class FilterVC: UIViewController {
 
+    @IBOutlet weak var majorView: UIView!
+    @IBOutlet weak var secondMajorView: UIView!
+    @IBOutlet weak var learnInfoView: UIView!
+    @IBOutlet weak var badClassView: UIView!
+    @IBOutlet weak var recommendClassView: UIView!
+    @IBOutlet weak var futureJobView: UIView!
+    @IBOutlet weak var tipView: UIView!
+    @IBOutlet weak var resetView: UIView!
+    @IBOutlet weak var completeBtn: NadoSunbaeBtn!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func tapDismissBtn(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension FilterVC {
+    private func configureUI() {
+        completeBtn.isActivated = false
+        completeBtn.setTitle("적용하기", for: .normal)
+        
+        majorView.makeRounded(cornerRadius: 12)
+        secondMajorView.makeRounded(cornerRadius: 12)
+        learnInfoView.makeRounded(cornerRadius: 12)
+        badClassView.makeRounded(cornerRadius: 12)
+        recommendClassView.makeRounded(cornerRadius: 12)
+        futureJobView.makeRounded(cornerRadius: 12)
+        tipView.makeRounded(cornerRadius: 12)
+        resetView.makeRounded(cornerRadius: 14)
+    }
 }
