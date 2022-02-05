e45665ca2305c41328ef6ec09d64c1d6a1a01f27
eunjuchoi <eunjuzzoo@naver.com>
2022-02-05 16:42:11 +0900

    ✨Feat: 필터 버튼 토글기능 및 적용하기 버튼 상태 변경 구현 (#157)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
@@ -8,39 +8,161 @@
 import UIKit
 
 class FilterVC: UIViewController {
+    
+    var isSelectedMajorBtn = false
+    var isSelectedSecondMajorBtn = false
+    var isSelectedLearnInfoBtn = false
+    var isSelectedBadClassBtn = false
+    var isSelectedRecommendClassBtn = false
+    var isSelectedFutureJobBtn = false
+    var isSelectedTipBtn = false
 
-    @IBOutlet weak var majorView: UIView!
-    @IBOutlet weak var secondMajorView: UIView!
-    @IBOutlet weak var learnInfoView: UIView!
-    @IBOutlet weak var badClassView: UIView!
-    @IBOutlet weak var recommendClassView: UIView!
-    @IBOutlet weak var futureJobView: UIView!
-    @IBOutlet weak var tipView: UIView!
-    @IBOutlet weak var resetView: UIView!
-    @IBOutlet weak var completeBtn: NadoSunbaeBtn!
+    @IBOutlet weak var completeBtn: NadoSunbaeBtn! {
+        didSet {
+            completeBtn.press {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var majorBtn: UIButton!
+    @IBOutlet weak var secondMajorBtn: UIButton!
+    @IBOutlet weak var learnInfoBtn: UIButton!
+    @IBOutlet weak var recommendClassBtn: UIButton!
+    @IBOutlet weak var badClassBtn: UIButton!
+    @IBOutlet weak var futureJobBtn: UIButton!
+    @IBOutlet weak var tipBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        configureDefaultUI()
     }
     
     @IBAction func tapDismissBtn(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func tapMajorBtn(_ sender: UIButton) {
+        isSelectedMajorBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapSecondMajorBtn(_ sender: UIButton) {
+        isSelectedSecondMajorBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapLearnInfoBtn(_ sender: UIButton) {
+        isSelectedLearnInfoBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapRecommendClassBtn(_ sender: UIButton) {
+        isSelectedRecommendClassBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapBadClassBtn(_ sender: UIButton) {
+        isSelectedBadClassBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapFutureJobBtn(_ sender: UIButton) {
+        isSelectedFutureJobBtn.toggle()
+        configureBtnUI()
+    }
+    
+    @IBAction func tapTipBtn(_ sender: UIButton) {
+        isSelectedTipBtn.toggle()
+        configureBtnUI()
+    }
+    
 }
 
 extension FilterVC {
-    private func configureUI() {
+    private func configureDefaultUI() {
         completeBtn.isActivated = false
         completeBtn.setTitle("적용하기", for: .normal)
         
-        majorView.makeRounded(cornerRadius: 12)
-        secondMajorView.makeRounded(cornerRadius: 12)
-        learnInfoView.makeRounded(cornerRadius: 12)
-        badClassView.makeRounded(cornerRadius: 12)
-        recommendClassView.makeRounded(cornerRadius: 12)
-        futureJobView.makeRounded(cornerRadius: 12)
-        tipView.makeRounded(cornerRadius: 12)
-        resetView.makeRounded(cornerRadius: 14)
+        majorBtn.makeRounded(cornerRadius: 12)
+        secondMajorBtn.makeRounded(cornerRadius: 12)
+        learnInfoBtn.makeRounded(cornerRadius: 12)
+        badClassBtn.makeRounded(cornerRadius: 12)
+        recommendClassBtn.makeRounded(cornerRadius: 12)
+        futureJobBtn.makeRounded(cornerRadius: 12)
+        tipBtn.makeRounded(cornerRadius: 12)
+        resetBtn.makeRounded(cornerRadius: 14.adjusted)
+    }
+    
+    private func configureBtnUI() {
+        if isSelectedMajorBtn == true {
+            majorBtn.backgroundColor = .mainLight
+            majorBtn.titleLabel?.font = .PretendardSB(size: 14)
+            majorBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedMajorBtn == false {
+            majorBtn.backgroundColor = .gray0
+            majorBtn.titleLabel?.font = .PretendardR(size: 14)
+            majorBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedSecondMajorBtn == true {
+            secondMajorBtn.backgroundColor = .mainLight
+            secondMajorBtn.titleLabel?.font = .PretendardSB(size: 14)
+            secondMajorBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedSecondMajorBtn == false {
+            secondMajorBtn.backgroundColor = .gray0
+            secondMajorBtn.titleLabel?.font = .PretendardR(size: 14)
+            secondMajorBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedLearnInfoBtn == true {
+            learnInfoBtn.backgroundColor = .mainLight
+            learnInfoBtn.titleLabel?.font = .PretendardSB(size: 14)
+            learnInfoBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedLearnInfoBtn == false {
+            learnInfoBtn.backgroundColor = .gray0
+            learnInfoBtn.titleLabel?.font = .PretendardR(size: 14)
+            learnInfoBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedRecommendClassBtn == true {
+            recommendClassBtn.backgroundColor = .mainLight
+            recommendClassBtn.titleLabel?.font = .PretendardSB(size: 14)
+            recommendClassBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedRecommendClassBtn == false {
+            recommendClassBtn.backgroundColor = .gray0
+            recommendClassBtn.titleLabel?.font = .PretendardR(size: 14)
+            recommendClassBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedBadClassBtn == true {
+            badClassBtn.backgroundColor = .mainLight
+            badClassBtn.titleLabel?.font = .PretendardSB(size: 14)
+            badClassBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedBadClassBtn == false {
+            badClassBtn.backgroundColor = .gray0
+            badClassBtn.titleLabel?.font = .PretendardR(size: 14)
+            badClassBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedFutureJobBtn == true {
+            futureJobBtn.backgroundColor = .mainLight
+            futureJobBtn.titleLabel?.font = .PretendardSB(size: 14)
+            futureJobBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedFutureJobBtn == false {
+            futureJobBtn.backgroundColor = .gray0
+            futureJobBtn.titleLabel?.font = .PretendardR(size: 14)
+            futureJobBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        if isSelectedTipBtn == true {
+            tipBtn.backgroundColor = .mainLight
+            tipBtn.titleLabel?.font = .PretendardSB(size: 14)
+            tipBtn.setTitleColor(UIColor.mainDefault, for: .normal)
+        } else if isSelectedTipBtn == false {
+            tipBtn.backgroundColor = .gray0
+            tipBtn.titleLabel?.font = .PretendardR(size: 14)
+            tipBtn.setTitleColor(UIColor.gray3, for: .normal)
+        }
+        
+        if isSelectedMajorBtn || isSelectedSecondMajorBtn || isSelectedLearnInfoBtn || isSelectedRecommendClassBtn || isSelectedBadClassBtn || isSelectedFutureJobBtn || isSelectedTipBtn == true {
+            completeBtn.isActivated = true
+        } else {
+            completeBtn.isActivated = false
+        }
     }
 }
