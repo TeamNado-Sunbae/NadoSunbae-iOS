3a40f2b87abce65f5b771707d4ec01f3c8f0b908
eunjuchoi <eunjuzzoo@naver.com>
2022-02-06 15:04:42 +0900

    ✅ Chore: 리뷰 반영한 코드 수정 (#157)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Xib/FilterVC.swift
@@ -14,15 +14,12 @@ class FilterVC: UIViewController {
     @IBOutlet weak var resetBtn: UIButton! {
         didSet {
             resetBtn.press {
-                self.isSelectedMajorBtn = false
-                self.isSelectedSecondMajorBtn = false
-                self.isSelectedLearnInfoBtn = false
-                self.isSelectedLearnInfoBtn = false
-                self.isSelectedBadClassBtn = false
-                self.isSelectedRecommendClassBtn = false
-                self.isSelectedFutureJobBtn = false
-                self.isSelectedTipBtn = false
-                self.configureBtnUI()
+                [self.majorBtn, self.secondMajorBtn, self.learnInfoBtn, self.recommendClassBtn, self.badClassBtn, self.futureJobBtn, self.tipBtn].forEach {
+                    UIButton in UIButton?.isSelected = false
+                }
+                [self.majorBtn, self.secondMajorBtn, self.learnInfoBtn, self.recommendClassBtn, self.badClassBtn, self.futureJobBtn, self.tipBtn].forEach {
+                    UIButton in self.setBtnStatus(btn: UIButton)
+                }
             }
         }
     }
@@ -35,13 +32,6 @@ class FilterVC: UIViewController {
     @IBOutlet weak var tipBtn: UIButton!
     
     // MARK: Properties
-    var isSelectedMajorBtn = false
-    var isSelectedSecondMajorBtn = false
-    var isSelectedLearnInfoBtn = false
-    var isSelectedBadClassBtn = false
-    var isSelectedRecommendClassBtn = false
-    var isSelectedFutureJobBtn = false
-    var isSelectedTipBtn = false
     var selectFilterDelegate: SendUpdateStatusDelegate?
     
     // MARK: Life Cycle
@@ -57,123 +47,69 @@ class FilterVC: UIViewController {
     }
     
     @IBAction func tapMajorBtn(_ sender: UIButton) {
-        isSelectedMajorBtn.toggle()
-        configureBtnUI()
+        majorBtn.isSelected.toggle()
+        setBtnStatus(btn: majorBtn)
     }
     
     @IBAction func tapSecondMajorBtn(_ sender: UIButton) {
-        isSelectedSecondMajorBtn.toggle()
-        configureBtnUI()
+        secondMajorBtn.isSelected.toggle()
+        setBtnStatus(btn: secondMajorBtn)
     }
     
     @IBAction func tapLearnInfoBtn(_ sender: UIButton) {
-        isSelectedLearnInfoBtn.toggle()
-        configureBtnUI()
+        learnInfoBtn.isSelected.toggle()
+        setBtnStatus(btn: learnInfoBtn)
     }
     
     @IBAction func tapRecommendClassBtn(_ sender: UIButton) {
-        isSelectedRecommendClassBtn.toggle()
-        configureBtnUI()
+        recommendClassBtn.isSelected.toggle()
+        setBtnStatus(btn: recommendClassBtn)
     }
     
     @IBAction func tapBadClassBtn(_ sender: UIButton) {
-        isSelectedBadClassBtn.toggle()
-        configureBtnUI()
+        badClassBtn.isSelected.toggle()
+        setBtnStatus(btn: badClassBtn)
     }
     
     @IBAction func tapFutureJobBtn(_ sender: UIButton) {
-        isSelectedFutureJobBtn.toggle()
-        configureBtnUI()
+        futureJobBtn.isSelected.toggle()
+        setBtnStatus(btn: futureJobBtn)
     }
     
     @IBAction func tapTipBtn(_ sender: UIButton) {
-        isSelectedTipBtn.toggle()
-        configureBtnUI()
+        tipBtn.isSelected.toggle()
+        setBtnStatus(btn: tipBtn)
     }
 }
 
 // MARK: - UI
 extension FilterVC {
     private func configureDefaultUI() {
+        [majorBtn, secondMajorBtn, learnInfoBtn, badClassBtn, recommendClassBtn, futureJobBtn, tipBtn].forEach {
+            UIButton in UIButton?.makeRounded(cornerRadius: 12)
+        }
+        resetBtn.makeRounded(cornerRadius: 14.adjusted)
+        
         completeBtn.isActivated = false
         completeBtn.setTitle("적용하기", for: .normal)
-        
-        majorBtn.makeRounded(cornerRadius: 12)
-        secondMajorBtn.makeRounded(cornerRadius: 12)
-        learnInfoBtn.makeRounded(cornerRadius: 12)
-        badClassBtn.makeRounded(cornerRadius: 12)
-        recommendClassBtn.makeRounded(cornerRadius: 12)
-        futureJobBtn.makeRounded(cornerRadius: 12)
-        tipBtn.makeRounded(cornerRadius: 12)
-        resetBtn.makeRounded(cornerRadius: 14.adjusted)
     }
     
-    private func configureBtnUI() {
-        if isSelectedMajorBtn == true {
-            majorBtn.backgroundColor = .mainLight
-            majorBtn.titleLabel?.font = .PretendardSB(size: 14)
-            majorBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedMajorBtn == false {
-            majorBtn.backgroundColor = .gray0
-            majorBtn.titleLabel?.font = .PretendardR(size: 14)
-            majorBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedSecondMajorBtn == true {
-            secondMajorBtn.backgroundColor = .mainLight
-            secondMajorBtn.titleLabel?.font = .PretendardSB(size: 14)
-            secondMajorBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedSecondMajorBtn == false {
-            secondMajorBtn.backgroundColor = .gray0
-            secondMajorBtn.titleLabel?.font = .PretendardR(size: 14)
-            secondMajorBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedLearnInfoBtn == true {
-            learnInfoBtn.backgroundColor = .mainLight
-            learnInfoBtn.titleLabel?.font = .PretendardSB(size: 14)
-            learnInfoBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedLearnInfoBtn == false {
-            learnInfoBtn.backgroundColor = .gray0
-            learnInfoBtn.titleLabel?.font = .PretendardR(size: 14)
-            learnInfoBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedRecommendClassBtn == true {
-            recommendClassBtn.backgroundColor = .mainLight
-            recommendClassBtn.titleLabel?.font = .PretendardSB(size: 14)
-            recommendClassBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedRecommendClassBtn == false {
-            recommendClassBtn.backgroundColor = .gray0
-            recommendClassBtn.titleLabel?.font = .PretendardR(size: 14)
-            recommendClassBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedBadClassBtn == true {
-            badClassBtn.backgroundColor = .mainLight
-            badClassBtn.titleLabel?.font = .PretendardSB(size: 14)
-            badClassBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedBadClassBtn == false {
-            badClassBtn.backgroundColor = .gray0
-            badClassBtn.titleLabel?.font = .PretendardR(size: 14)
-            badClassBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedFutureJobBtn == true {
-            futureJobBtn.backgroundColor = .mainLight
-            futureJobBtn.titleLabel?.font = .PretendardSB(size: 14)
-            futureJobBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedFutureJobBtn == false {
-            futureJobBtn.backgroundColor = .gray0
-            futureJobBtn.titleLabel?.font = .PretendardR(size: 14)
-            futureJobBtn.setTitleColor(UIColor.gray3, for: .normal)
-        }
-        if isSelectedTipBtn == true {
-            tipBtn.backgroundColor = .mainLight
-            tipBtn.titleLabel?.font = .PretendardSB(size: 14)
-            tipBtn.setTitleColor(UIColor.mainDefault, for: .normal)
-        } else if isSelectedTipBtn == false {
-            tipBtn.backgroundColor = .gray0
-            tipBtn.titleLabel?.font = .PretendardR(size: 14)
-            tipBtn.setTitleColor(UIColor.gray3, for: .normal)
+    /// 버튼 UI 설정해주는 함수
+    private func configureBtnUI(btn: UIButton, btnBgColor: UIColor, titleFont: UIFont, btnTitleColor: UIColor) {
+        btn.backgroundColor = btnBgColor
+        btn.titleLabel?.font = titleFont
+        btn.setTitleColor(btnTitleColor, for: .normal)
+    }
+    
+    /// 버튼 상태에 따른 UI 설정 함수
+    private func setBtnStatus(btn: UIButton) {
+        if btn.isSelected == true {
+            configureBtnUI(btn: btn, btnBgColor: .mainLight, titleFont: .PretendardSB(size: 14), btnTitleColor: .mainDefault)
+        } else {
+            configureBtnUI(btn: btn, btnBgColor: .gray0, titleFont: .PretendardR(size: 14), btnTitleColor: .gray3)
         }
         
-        if isSelectedMajorBtn || isSelectedSecondMajorBtn || isSelectedLearnInfoBtn || isSelectedRecommendClassBtn || isSelectedBadClassBtn || isSelectedFutureJobBtn || isSelectedTipBtn == true {
+        if majorBtn.isSelected || secondMajorBtn.isSelected || learnInfoBtn.isSelected || recommendClassBtn.isSelected || badClassBtn.isSelected || futureJobBtn.isSelected || tipBtn.isSelected == true {
             completeBtn.isActivated = true
         } else {
             completeBtn.isActivated = false
