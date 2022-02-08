29fb52304ab368f00c9b46ca3da4d1a71078ed83
Jungbin Jung <lollin0@naver.com>
2022-02-08 13:25:49 +0900

    ✨ Feat: 학과 정보 리스트 조회 함수 구현 (#155)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/SignUpMajorInfo/VC/SelectMajorModalVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/SignUpMajorInfo/VC/SelectMajorModalVC.swift
@@ -17,8 +17,8 @@ class SelectMajorModalVC: BaseVC {
     /// 진입된 버튼의 태그, 0: 본전공, 1: 본전공진입시기, 2: 제2전공, 3: 제2전공진입시기
     var enterdBtnTag = 0
     
-    var majorList = ["나도학과", "선배학과"]
     var startList = ["19-1", "19-2", "20-1", "20-2"]
+    var majorList: [MajorInfoModel] = []
     var selectMajorDelegate: SendUpdateModalDelegate?
     
     // MARK: LifeCycle
@@ -32,14 +32,16 @@ class SelectMajorModalVC: BaseVC {
     private func configureUI() {
         completeBtn.setTitleWithStyle(title: "선택 완료", size: 16, weight: .semiBold)
         completeBtn.isEnabled = false
-        /// enterBtnTag에 맞춰서 타이틀 변경
+        /// enterBtnTag에 맞춰서 타이틀, 데이터 변경
         titleLabel.text = { () -> String in
             switch enterdBtnTag {
             case 0:
+                requestGetMajorList(univID: 1, filterType: "firstMajor")
                 return "본전공"
             case 1:
                 return "본전공 진입시기"
             case 2:
+                requestGetMajorList(univID: 1, filterType: "secondMajor")
                 return "제2전공"
             case 3:
                 return "제2전공 진입시기"
@@ -72,5 +74,32 @@ class SelectMajorModalVC: BaseVC {
     
     @IBAction func tapDismissBtn(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SelectMajorModalVC {
+    
+    /// 학과 정보 리스트 조회
+    func requestGetMajorList(univID: Int, filterType: String) {
+        PublicAPI.shared.getMajorListAPI(univID: univID, filterType: filterType) { networkResult in
+            switch networkResult {
+            case .success(let res):
+                DispatchQueue.main.async {
+                    if let data = res as? [MajorListData] {
+                        for i in 0...data.count - 3 {
+                            self.majorList.append(MajorInfoModel(majorID: data[i].majorID, majorName: data[i].majorName))
+                        }
+                        self.selectMajorTV.reloadData()
+                    }
+                }
+            case .requestErr(let msg):
+                if let message = msg as? String {
+                    print(message)
+                    self.makeAlert(title: "네트워크 오류로 인해\n데이터를 불러올 수 없습니다.\n다시 시도해 주세요.")
+                }
+            default:
+                self.makeAlert(title: "네트워크 오류로 인해\n데이터를 불러올 수 없습니다.\n다시 시도해 주세요.")
+            }
+        }
     }
 }
