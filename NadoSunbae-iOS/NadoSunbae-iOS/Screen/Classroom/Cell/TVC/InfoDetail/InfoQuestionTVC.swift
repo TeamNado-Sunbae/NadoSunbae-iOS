c2f3100d13be62eb2b0339aa3c1701eda2abe6f2
hwangJi-dev <melon2228@swu.ac.kr>
2022-02-14 02:03:32 +0900

    ✅ Chore: 과방탭 정보글 UI 수정 (#125)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/InfoDetail/InfoQuestionTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/InfoDetail/InfoQuestionTVC.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class InfoQuestionTVC: UITableViewCell {
-
+    
+    // MARK: Properties
     @IBOutlet weak var infoTitleLabel: UILabel!
     @IBOutlet weak var nicknameLabel: UILabel!
     @IBOutlet weak var majorInfoLabel: UILabel!
     @IBOutlet weak var questionContentTextView: UITextView!
     @IBOutlet weak var postDateLabel: UILabel!
+    @IBOutlet weak var infoLikeBtn: UIButton!
+    @IBOutlet weak var infoLikeImgView: UIImageView!
+    @IBOutlet weak var infoLikeCountLabel: UILabel!
     
+    override var isSelected: Bool {
+        didSet {
+            infoLikeBtn.backgroundColor = isSelected ? UIColor.nadoBlack : UIColor.gray0
+            infoLikeImgView.image = isSelected ? UIImage(named: "heart_mint") : UIImage(named: "heart")
+        }
+    }
+    
+    // MARK: Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureUI()
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: IBAction
+    @IBAction func tapInfoLikeBtn(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+    }
+}
+
+// MARK: - UI
+extension InfoQuestionTVC {
+    private func configureUI() {
+        infoLikeBtn.layer.cornerRadius = 16
+    }
 }
