7a5edf4df0b95f0f756f630de67afc97dfe2a614
hwangJi-dev <melon2228@swu.ac.kr>
2022-02-13 16:34:31 +0900

    ➕ Add: 과방탭 정보글 UI 생성에 필요한 파일 추가 (#125)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/InfoDetail/InfoCommentHeaderTVC.swift
@@ -0,0 +1,23 @@
+//
+//  InfoCommentHeaderTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/02/13.
+//
+
+import UIKit
+
+class InfoCommentHeaderTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/InfoDetail/InfoQuestionTVC.swift
@@ -0,0 +1,23 @@
+//
+//  InfoQuestionTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/02/13.
+//
+
+import UIKit
+
+class InfoQuestionTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
