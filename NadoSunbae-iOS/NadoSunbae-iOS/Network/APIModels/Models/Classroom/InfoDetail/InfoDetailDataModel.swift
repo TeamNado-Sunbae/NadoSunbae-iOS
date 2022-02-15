ca2b46db53a58c8119b833336de12fc8c5065c1c
hwangJi-dev <melon2228@swu.ac.kr>
2022-02-15 17:47:11 +0900

    ✅ Chore: 파일 위치 변경 및 필요없는 파일 삭제 (#125)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Classroom/InfoDetail/InfoDetailDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  InfoDetailDataModel.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/02/15.
+//
+
+import Foundation
--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Classroom/Question/ClassroomQuestionDetailData.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Classroom/Question/ClassroomQuestionDetailData.swift
@@ -62,3 +62,4 @@ func convertToMajorInfoString(_ firstMajorName: String, _ firstMajorStart: Strin
         return firstMajorName + " " + firstMajorStart + " " + "|" + " " + secondMajorName + " " + secondMajorStart
     }
 }
+
