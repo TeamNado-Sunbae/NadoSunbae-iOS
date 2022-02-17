8912daee2ba1b04311aacc3f1be0bca4a981be2d
Jungbin Jung <lollin0@naver.com>
2022-02-18 01:03:42 +0900

    ✅ Chore: 마이페이지 내가 쓴 글_질문 TV 코드 작성 (#166)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageQuestionListVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageQuestionListVC.swift
@@ -15,14 +15,19 @@ class MypageQuestionListVC: BaseVC {
             questionSegmentView.backgroundColor = .bgGray
         }
     }
+    @IBOutlet weak var questionTV: UITableView!
+    @IBOutlet weak var questionTVHeight: NSLayoutConstraint!
     
     // MARK: Properties
     weak var sendSegmentStateDelegate: SendSegmentStateDelegate?
+    var questionList: [MypageMyPostModel] = []
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTapInfoBtn()
+        setUpTV()
+        registerCell()
     }
 }
 
@@ -35,6 +40,51 @@ extension MypageQuestionListVC {
             }
         }
     }
+    
+    func setUpTV() {
+        questionTV.delegate = self
+        questionTV.dataSource = self
+    }
+    
+    private func registerCell() {
+        questionTV.register(MypagePostListTVC.self, forCellReuseIdentifier: MypagePostListTVC.className)
+    }
 }
 
+// MARK: UITableViewDataSource
+extension MypageQuestionListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MypagePostListTVC.className, for: indexPath) as? MypagePostListTVC else { return UITableViewCell() }
+        cell.setMypageMyPostData(data: self.questionList[indexPath.row])
+        cell.layoutIfNeeded()
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MypageQuestionListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let chatSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionChatSB, bundle: nil)
+        guard let chatVC = chatSB.instantiateViewController(identifier: DefaultQuestionChatVC.className) as? DefaultQuestionChatVC else { return }
+        
+        chatVC.questionType = .group
+        chatVC.naviStyle = .push
+        chatVC.chatPostID = self.questionList[indexPath.row].postID
+        
+        self.navigationController?.pushViewController(chatVC, animated: true)
+    }
+}
 
