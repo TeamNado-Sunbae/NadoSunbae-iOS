2db4f9475670e5bfe04e38f367eda156aedc1117
Jungbin Jung <lollin0@naver.com>
2022-02-22 01:13:54 +0900

    ✅ Chore: 마이페이지 학과후기 title 디자인 적용(#166)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageMyReviewVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageMyReviewVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MypageMyReviewVC: UIViewController {
+class MypageMyReviewVC: BaseVC {
 
     // MARK: @IBOutlet
     @IBOutlet weak var navView: NadoSunbaeNaviBar! {
@@ -19,9 +19,37 @@ class MypageMyReviewVC: UIViewController {
             }
         }
     }
+    @IBOutlet weak var titleLabel: UILabel!
     
     // MARK: Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getMypageMyReview()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+}
+
+// MARK: - UI
+extension MypageMyReviewVC {
+    private func configureUI() {
+        setTitleLabel()
+    }
+    
+    private func setTitleLabel() {
+        let attributedString = NSMutableAttributedString(string: "\("내 닉네임")님이 쓴 학과 후기")
+        let text = "\("내 닉네임")님이 쓴 학과 후기"
+        attributedString.addAttribute(.foregroundColor, value: UIColor.mainDefault, range: (text as NSString).range(of: "내 닉네임"))
+        titleLabel.attributedText = attributedString
+    }
+}
+
+// MARK: - Network
+extension MypageMyReviewVC {
+    private func getMypageMyReview() {
+        
     }
 }
