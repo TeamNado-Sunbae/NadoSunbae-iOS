5734def1fcf66a0edab85be84a59b43dfda7d4dd
Jungbin Jung <lollin0@naver.com>
2022-02-22 02:36:31 +0900

    ✅ Chore: 학과후기 UI 코드 작성 (#166)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageMyReviewVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypageMyReviewVC.swift
@@ -20,6 +20,12 @@ class MypageMyReviewVC: BaseVC {
         }
     }
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var reviewTV: UITableView!
+    @IBOutlet weak var reviewTVHeight: NSLayoutConstraint!
+    @IBOutlet weak var emptyView: UIView!
+    
+    // MARK: Properties
+    var reviewList: [MypageMyReviewModel] = []
     
     // MARK: Life Cycle
     override func viewWillAppear(_ animated: Bool) {
@@ -37,19 +43,46 @@ class MypageMyReviewVC: BaseVC {
 extension MypageMyReviewVC {
     private func configureUI() {
         setTitleLabel()
+        emptyView.makeRounded(cornerRadius: 8.adjusted)
     }
     
     private func setTitleLabel() {
-        let attributedString = NSMutableAttributedString(string: "\("내 닉네임")님이 쓴 학과 후기")
-        let text = "\("내 닉네임")님이 쓴 학과 후기"
-        attributedString.addAttribute(.foregroundColor, value: UIColor.mainDefault, range: (text as NSString).range(of: "내 닉네임"))
+        let userName = "내 닉네임"
+        let titleText = "\(userName)님이 쓴 학과 후기"
+        let attributedString = NSMutableAttributedString(string: titleText)
+        attributedString.addAttribute(.foregroundColor, value: UIColor.mainDefault, range: (titleText as NSString).range(of: userName))
         titleLabel.attributedText = attributedString
     }
+    
+    private func setEmptyView() {
+        emptyView.isHidden = !(reviewList.isEmpty)
+    }
+    
+    private func setUpTV() {
+        reviewTV.delegate = self
+        reviewTV.dataSource = self
+        reviewTVHeight.constant = reviewTV.contentSize.height
+    }
+}
+
+// MARK: -
+extension MypageMyReviewVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reviewList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+extension MypageMyReviewVC: UITableViewDelegate {
+    
 }
 
 // MARK: - Network
 extension MypageMyReviewVC {
     private func getMypageMyReview() {
-        
+        setEmptyView()
     }
 }
