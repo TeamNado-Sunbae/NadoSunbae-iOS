7aa50a100bd4fdc9043cff8d8b68faf4796d1438
hwangJi-dev <melon2228@swu.ac.kr>
2022-02-23 16:56:32 +0900

    ✅ Chore: NadoTextView -> placeholder를 사용하는지 아닌지 Bool 상태로 받도록 함수 수정 (#207)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoTextView.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoTextView.swift
@@ -14,14 +14,19 @@ import Then
  */
 class NadoTextView: UITextView {
     
-    func setDefaultStyle(placeholderText: String) {
+    func setDefaultStyle(isUsePlaceholder: Bool, placeholderText: String) {
         self.layer.cornerRadius = 8
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.gray0.cgColor
-        self.text = placeholderText
-        self.textColor = .gray2
         self.font = .PretendardR(size: 14.0)
         self.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        if isUsePlaceholder {
+            self.text = placeholderText
+            self.textColor = .gray2
+        } else {
+            self.textColor = .mainText
+        }
         
         let style = NSMutableParagraphStyle()
         style.lineSpacing = 6
