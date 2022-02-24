41a2b32070d5786da470d1804c1e9a81684496c8
dev-madilyn <lollin0@naver.com>
2022-02-25 01:15:00 +0900

    ✨ Feat: 프로필 수정 뷰 UI 구현 (#214)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ProfileSetting/VC/EditProfileVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ProfileSetting/VC/EditProfileVC.swift
@@ -20,9 +20,35 @@ class EditProfileVC: BaseVC {
             }
         }
     }
+    @IBOutlet weak var profileImgView: UIImageView!
+    @IBOutlet weak var nickNameChangeBtn: UIButton!
+    @IBOutlet weak var nickNameRuleLabel: UILabel!
+    @IBOutlet weak var nickNameTextField: NadoTextField!
+    @IBOutlet weak var nickNameInfoLabel: UILabel!
+    @IBOutlet weak var isQuestionOnToggleBtn: UIButton! {
+        didSet {
+            isQuestionOnToggleBtn.setImage(UIImage(named: "toggle_off"), for: .normal)
+            isQuestionOnToggleBtn.setImage(UIImage(named: "toggle_on"), for: .selected)
+            isQuestionOnToggleBtn.press {
+                self.isQuestionOnToggleBtn.isSelected.toggle()
+            }
+        }
+    }
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: Custom Methods
+}
+
+// MARK: - UI
+extension EditProfileVC {
+    
+}
+
+// MARK:- Network
+extension EditProfileVC {
+    
 }
