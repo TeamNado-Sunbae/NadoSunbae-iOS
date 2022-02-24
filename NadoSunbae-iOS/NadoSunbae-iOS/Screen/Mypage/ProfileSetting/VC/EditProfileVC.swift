1da79b551a1f94ffdfcd222e574b68691b7c367c
dev-madilyn <lollin0@naver.com>
2022-02-25 02:00:18 +0900

    Chore: 정보 불러오기 API 연결 및 ConfigureUI 함수 작성 (#214)): Chore: 정보 불러오기 API 연결 및 ConfigureUI 함수 작성 (#214)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ProfileSetting/VC/EditProfileVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ProfileSetting/VC/EditProfileVC.swift
@@ -25,19 +25,31 @@ class EditProfileVC: BaseVC {
     @IBOutlet weak var nickNameRuleLabel: UILabel!
     @IBOutlet weak var nickNameTextField: NadoTextField!
     @IBOutlet weak var nickNameInfoLabel: UILabel!
-    @IBOutlet weak var isQuestionOnToggleBtn: UIButton! {
+    @IBOutlet weak var isOnQuestionToggleBtn: UIButton! {
         didSet {
-            isQuestionOnToggleBtn.setImage(UIImage(named: "toggle_off"), for: .normal)
-            isQuestionOnToggleBtn.setImage(UIImage(named: "toggle_on"), for: .selected)
-            isQuestionOnToggleBtn.press {
-                self.isQuestionOnToggleBtn.isSelected.toggle()
+            isOnQuestionToggleBtn.setImage(UIImage(named: "toggle_off"), for: .normal)
+            isOnQuestionToggleBtn.setImage(UIImage(named: "toggle_on"), for: .selected)
+            isOnQuestionToggleBtn.press {
+                self.isOnQuestionToggleBtn.isSelected.toggle()
             }
         }
     }
+    @IBOutlet weak var firstMajorTextField: NadoTextField!
+    @IBOutlet weak var firstMajorStartTextField: NadoTextField!
+    @IBOutlet weak var secondMajorTextField: NadoTextField!
+    @IBOutlet weak var secondMajorStartTextField: NadoTextField!
+    
+    // MARK: Properties
+    var userInfo = MypageUserInfoModel()
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getMyInfo()
     }
     
     // MARK: Custom Methods
@@ -45,10 +57,39 @@ class EditProfileVC: BaseVC {
 
 // MARK: - UI
 extension EditProfileVC {
-    
+    private func configureUI() {
+        profileImgView.image = UIImage(named: "profileImage\(userInfo.profileImageID)")
+        nickNameTextField.placeholder = userInfo.nickname
+        isOnQuestionToggleBtn.isSelected = userInfo.isOnQuestion
+        firstMajorTextField.text = userInfo.firstMajorName
+        firstMajorStartTextField.text = userInfo.firstMajorStart
+        secondMajorTextField.text = userInfo.secondMajorName
+        secondMajorStartTextField.text = userInfo.secondMajorStart
+    }
 }
 
 // MARK:- Network
 extension EditProfileVC {
-    
+    private func getMyInfo() {
+        self.activityIndicator.startAnimating()
+        MypageAPI.shared.getUserInfo(userID: UserDefaults.standard.integer(forKey: UserDefaults.Keys.UserID), completion: { networkResult in
+            switch networkResult {
+            case .success(let res):
+                if let data = res as? MypageUserInfoModel {
+                    self.userInfo = data
+                    self.configureUI()
+                }
+                self.activityIndicator.stopAnimating()
+            case .requestErr(let msg):
+                if let message = msg as? String {
+                    print(message)
+                }
+                self.activityIndicator.stopAnimating()
+                self.makeAlert(title: "네트워크 오류로 인해\n데이터를 불러올 수 없습니다.\n다시 시도해 주세요.")
+            default:
+                self.activityIndicator.stopAnimating()
+                self.makeAlert(title: "네트워크 오류로 인해\n데이터를 불러올 수 없습니다.\n다시 시도해 주세요.")
+            }
+        })
+    }
 }
