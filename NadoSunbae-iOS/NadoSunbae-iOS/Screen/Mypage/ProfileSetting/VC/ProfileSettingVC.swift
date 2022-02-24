14b4cc08aa81795f0f5a272dc210e6a3d2d6a969
dev-madilyn <lollin0@naver.com>
2022-02-24 23:59:27 +0900

    ✅ Chore: ProfileSettingVC 생성, nav View 세팅(#214)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ProfileSetting/VC/ProfileSettingVC.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileSettingVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by madilyn on 2022/02/24.
+//
+
+import UIKit
+
+class ProfileSettingVC: BaseVC {
+    
+    // MARK: @IBOutlet
+    @IBOutlet weak var navView: NadoSunbaeNaviBar! {
+        didSet {
+            navView.setUpNaviStyle(state: .backDefaultWithNadoBtn)
+            navView.configureTitleLabel(title: "프로필 수정")
+            navView.rightActivateBtn.setTitleWithStyle(title: "저장", size: 14)
+            navView.backBtn.press {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
