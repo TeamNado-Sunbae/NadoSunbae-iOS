432689f117608e5431004f7353b832a22aa818f8
dev-madilyn <lollin0@naver.com>
2022-02-28 02:34:54 +0900

    ➕ Add: 최신 버전 받기 Model 생성 (#168)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Mypage/MypageSetting/GetLatestVersionResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  GetLatestVersionResponseModel.swift
+//  NadoSunbae-iOS
+//
+//  Created by madilyn on 2022/02/28.
+//
+
+import Foundation
+
+struct GetLatestVersionResponseModel: Codable {
+    var AOS, iOS: String
+}
