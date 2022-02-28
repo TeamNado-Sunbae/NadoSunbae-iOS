77d4a039d0ae0ad71faa143a46d6d0072330a25f
dev-madilyn <lollin0@naver.com>
2022-03-01 03:48:19 +0900

    ✅ Chore: 비밀번호 재설정 성공 뷰 버튼 세팅 (#231)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWCompleteVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWCompleteVC.swift
@@ -9,7 +9,30 @@ import UIKit
 
 class ResetPWCompleteVC: BaseVC {
     
+    @IBOutlet weak var goSignInBtn: NadoSunbaeBtn! {
+        didSet {
+            goSignInBtn.isActivated = true
+            goSignInBtn.isEnabled = true
+            goSignInBtn.press {
+                // TODO: 로그아웃 요청
+            }
+        }
+    }
+    @IBOutlet weak var resendBtn: UIButton! {
+        didSet {
+            resendBtn.press {
+                // TODO: 재전송 요청
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        hideTabbar()
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
 }
