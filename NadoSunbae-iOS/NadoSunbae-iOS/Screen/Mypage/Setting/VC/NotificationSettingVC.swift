6aa0031334e10dc4e7ee0b8303fc9f0a069f6b75
dev-madilyn <lollin0@naver.com>
2022-02-28 19:15:37 +0900

    ✨ Feat: 알림설정 뷰 UI 구성 (#230)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/Setting/VC/NotificationSettingVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/Setting/VC/NotificationSettingVC.swift
@@ -19,10 +19,35 @@ class NotificationSettingVC: BaseVC {
             }
         }
     }
+    @IBOutlet weak var settingTV: UITableView! {
+        didSet {
+            settingTV.dataSource = self
+            settingTV.rowHeight = 64.adjustedH
+            settingTV.separatorStyle = .none
+            settingTV.allowsSelection = false
+        }
+    }
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        registerXIB()
+    }
+    
+    // MARK: Custom Methods
+    private func registerXIB() {
+        NotificationSettingBasicTVC.register(target: settingTV)
+    }
+}
+
+extension NotificationSettingVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationSettingBasicTVC.className, for: indexPath) as? NotificationSettingBasicTVC else { return UITableViewCell() }
+        cell.setData(title: "알림", isOn: true)
+        return cell
     }
 }
