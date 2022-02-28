d0c68419e46e6a8c969191a393dc43e9984bb56d
dev-madilyn <lollin0@naver.com>
2022-02-28 23:03:17 +0900

    ➕ Add: 비밀번호 찾기, 비밀번호 찾기 완료 VC, SB 생성 및 연결 (#231)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWCompleteVC.swift
@@ -0,0 +1,15 @@
+//
+//  ResetPWCompleteVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by madilyn on 2022/02/28.
+//
+
+import UIKit
+
+class ResetPWCompleteVC: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWVC.swift
@@ -0,0 +1,15 @@
+//
+//  ResetPWVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by madilyn on 2022/02/28.
+//
+
+import UIKit
+
+class ResetPWVC: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
