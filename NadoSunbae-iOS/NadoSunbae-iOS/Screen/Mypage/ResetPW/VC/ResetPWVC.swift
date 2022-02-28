68a13df1e83e1846a1d99875a36083b642d4f6ea
dev-madilyn <lollin0@naver.com>
2022-03-01 02:04:23 +0900

    ✅ Chore: 비밀번호 재설정 UI 구성 (#231)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/ResetPW/VC/ResetPWVC.swift
@@ -6,10 +6,44 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ResetPWVC: BaseVC {
     
+    // MARK: @IBOutlet
+    @IBOutlet weak var backBtn: UIButton! {
+        didSet {
+            backBtn.press {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    @IBOutlet weak var contentLabel: UILabel! {
+        didSet {
+            contentLabel.text = """
+                                본인 인증을 위해
+                                가입 당시 입력한 정보를 입력해주세요.
+                                """
+        }
+    }
+    @IBOutlet weak var emailTextField: NadoTextField!
+    @IBOutlet weak var emailClearBtn: UIButton!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var confirmBtn: NadoSunbaeBtn!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        hideTabbar()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        showTabbar()
     }
 }
