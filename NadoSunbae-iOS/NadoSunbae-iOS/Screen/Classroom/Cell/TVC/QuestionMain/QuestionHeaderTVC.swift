ecefab86bfbb1f73340f00b2e64ddd3f545343dc
hwangJi-dev <melon2228@swu.ac.kr>
2022-03-03 21:06:47 +0900

    ✅ Chore: QuestionHeaderTVC UI 수정 (#249)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/QuestionMain/QuestionHeaderTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/TVC/QuestionMain/QuestionHeaderTVC.swift
@@ -10,28 +10,11 @@ import UIKit
 class QuestionHeaderTVC: BaseTVC {
     
     // MARK: Properties
-    private let headerTitleLabel = UILabel().then {
-        $0.text = "구성원 모두가 답할 수 있어요!"
-        $0.textColor = .mainDefault
-        $0.font = .PretendardR(size: 14.0)
-        $0.sizeToFit()
-    }
-    
     private let questionWriteBtn = UIButton().then {
-        $0.setBackgroundImage(UIImage(named: "btnWrite"), for: .normal)
+        $0.setBackgroundImage(UIImage(named: "goToQuestion"), for: .normal)
         $0.contentMode = .scaleAspectFill
     }
     var tapWriteBtnAction : (() -> ())?
-    
-    // MARK: Life Cycle
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 12, left: 16, bottom: 11, right: 16))
-    }
     
     // MARK: init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -50,19 +33,13 @@ class QuestionHeaderTVC: BaseTVC {
 // MARK: - UI
 extension QuestionHeaderTVC {
     private func configureUI() {
-        contentView.addSubviews([headerTitleLabel, questionWriteBtn])
+        contentView.addSubview(questionWriteBtn)
         
         questionWriteBtn.snp.makeConstraints {
-            $0.trailing.equalToSuperview()
-            $0.height.equalTo(36)
-            $0.width.equalTo(104)
+            $0.trailing.equalTo(contentView).offset(-8)
+            $0.height.equalTo(36.adjustedH)
+            $0.width.equalTo(104.adjusted)
             $0.centerY.equalTo(contentView)
-        }
-        
-        headerTitleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview()
-            $0.trailing.equalTo(questionWriteBtn.snp.leading).offset(-30)
-            $0.centerY.equalTo(questionWriteBtn)
         }
     }
 }
