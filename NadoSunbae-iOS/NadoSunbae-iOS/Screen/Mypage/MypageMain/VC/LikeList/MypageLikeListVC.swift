a17015d1e598cae38297176c9668527314ebb428
hwangJi-dev <melon2228@swu.ac.kr>
2022-03-07 04:10:55 +0900

    ➕ Add: 좋아요 목록 VC 생성 (#281)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageMain/VC/LikeList/MypageLikeListVC.swift
@@ -0,0 +1,20 @@
+//
+//  MypageLikeListVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/03/07.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MypageLikeListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
