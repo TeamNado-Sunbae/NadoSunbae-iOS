cda7bd14f94faf0027c0f78c20f8dcfd4c0976b0
eunjuchoi <eunjuzzoo@naver.com>
2022-03-09 00:49:36 +0900

    ➕ Add: 뷰 하단 간격 위한 TVC 생성 (#303)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Cell/TVC/ReviewDetailGrayTVC.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewDetailGrayTVC.swift
+//  NadoSunbae-iOS
+//
+//  Created by EUNJU on 2022/03/09.
+//
+
+import UIKit
+
+class ReviewDetailGrayTVC: BaseTVC {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
