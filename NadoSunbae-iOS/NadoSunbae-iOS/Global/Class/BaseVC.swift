89e1ec3afe74c82bfbd76f16916dc92e86c77647
dev-madilyn <lollin0@naver.com>
2022-03-09 00:25:16 +0900

    ✅ Chore: 토큰을 싱글톤에 저장하는 메서드 구현 (#297)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
@@ -51,6 +51,12 @@ extension BaseVC {
         view.addGestureRecognizer(tap)
     }
     
+    /// 로그인/자동로그인 시 토큰을 싱글톤에 저장하는 메서드
+    func setUserToken(accessToken: String, refreshToken: String) {
+        UserToken.shared.accessToken = accessToken
+        UserToken.shared.refreshToken = refreshToken
+    }
+    
     /// 토큰 갱신, 자동로그인 시 UserDefaults에 값 저장하는 메서드
     func setUpUserdefaultValues(data: SignInDataModel) {
         UserDefaults.standard.set(data.accesstoken, forKey: UserDefaults.Keys.AccessToken)
