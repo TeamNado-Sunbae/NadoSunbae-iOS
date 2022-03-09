2fcc66774c478ad4807486606ddd8c6f29b42f16
eunjuchoi <eunjuzzoo@naver.com>
2022-03-09 19:39:33 +0900

    ✨ Feat: 선배 마이페이지에서 선배에게 온 질문 열람 제한 (#324)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageUser/VC/MypageUserVC+TV.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypageUser/VC/MypageUserVC+TV.swift
@@ -33,14 +33,30 @@ extension MypageUserVC: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let chatSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionChatSB, bundle: nil)
-        guard let personalChatVC = chatSB.instantiateViewController(identifier: DefaultQuestionChatVC.className) as? DefaultQuestionChatVC else { return }
-       
-        personalChatVC.questionType = .personal
-        personalChatVC.naviStyle = .push
-        personalChatVC.postID = self.questionList[indexPath.row].postID
-        personalChatVC.hidesBottomBarWhenPushed = true
         
-        self.navigationController?.pushViewController(personalChatVC, animated: true)
+        /// 후기글 작성하지 않은 유저라면 후기글 열람 제한
+        if !(UserDefaults.standard.bool(forKey: UserDefaults.Keys.IsReviewed)) {
+            guard let restrictionAlert = Bundle.main.loadNibNamed(NadoAlertVC.className, owner: self, options: nil)?.first as? NadoAlertVC else { return }
+            
+            /// 후기 작성 버튼 클릭시 후기 작성 페이지로 이동
+            restrictionAlert.confirmBtn.press {
+                let ReviewWriteSB = UIStoryboard.init(name: "ReviewWriteSB", bundle: nil)
+                guard let nextVC = ReviewWriteSB.instantiateViewController(withIdentifier: ReviewWriteVC.className) as? ReviewWriteVC else { return }
+                
+                nextVC.modalPresentationStyle = .fullScreen
+                self.present(nextVC, animated: true, completion: nil)
+            }
+            restrictionAlert.showNadoAlert(vc: self, message: "내 학과 후기를 작성해야\n이용할 수 있는 기능이에요.", confirmBtnTitle: "후기 작성", cancelBtnTitle: "다음에 작성")
+        } else {
+            let chatSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionChatSB, bundle: nil)
+            guard let personalChatVC = chatSB.instantiateViewController(identifier: DefaultQuestionChatVC.className) as? DefaultQuestionChatVC else { return }
+            
+            personalChatVC.questionType = .personal
+            personalChatVC.naviStyle = .push
+            personalChatVC.postID = self.questionList[indexPath.row].postID
+            personalChatVC.hidesBottomBarWhenPushed = true
+            
+            self.navigationController?.pushViewController(personalChatVC, animated: true)
+        }
     }
 }
