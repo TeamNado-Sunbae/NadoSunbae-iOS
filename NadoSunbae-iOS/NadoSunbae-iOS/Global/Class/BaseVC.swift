b09e00d792ba05f51e1843282361df21e921e093
dev-madilyn <lollin0@naver.com>
2022-03-09 09:52:38 +0900

    🔨 Fix: 액세스토큰 갱신 시 세팅 오류 해결 (#313)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
@@ -147,7 +147,7 @@ extension BaseVC {
             switch networkResult {
             case .success(let res):
                 if let data = res as? SignInDataModel {
-                    self.setUserToken(accessToken: data.accesstoken, refreshToken: data.accesstoken)
+                    self.setUserToken(accessToken: data.accesstoken, refreshToken: data.refreshtoken)
                     self.setUpUserdefaultValues(data: data)
                     completion(true)
                 }
