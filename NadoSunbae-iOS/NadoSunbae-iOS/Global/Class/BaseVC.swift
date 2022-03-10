00d2e25298628702c5aea5cbe16557eacd1ea9db
hwangJi-dev <melon2228@swu.ac.kr>
2022-03-11 01:31:30 +0900

    ✅ Chore: 유저 권한 분기처리 코드 수정 (#337)

--- NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Class/BaseVC.swift
@@ -115,9 +115,20 @@ extension BaseVC {
         guard let restrictionAlert = Bundle.main.loadNibNamed(NadoAlertVC.className, owner: self, options: nil)?.first as? NadoAlertVC else { return }
         
         switch permissionStatus {
-        case .review, .inappropriate:
+        case .review:
             restrictionAlert.confirmBtn.press {
                 self.presentToReviewWriteVC { _ in }
+            }
+        case .inappropriate:
+            permissionMsg = "부적절한 후기 작성이 확인되어\n열람 권한이 제한되었습니다.\n권한을 얻고 싶다면\n다시 학과후기를 작성해주세요."
+            comfirmTitle = "문의하기"
+            cancelTitle = "닫기"
+            restrictionAlert.confirmBtn.press {
+                self.getAppLink { appLink in
+                    if let url = URL(string: appLink.kakaoTalkChannel) {
+                        UIApplication.shared.open(url, options: [:])
+                    }
+                }
             }
         case .report:
             permissionMsg = UserPermissionInfo.shared.permissionMsg
@@ -139,9 +150,7 @@ extension BaseVC {
     func divideUserPermission(defaultAction: () -> Void) {
         if UserPermissionInfo.shared.isUserReported {
             self.showRestrictionAlert(permissionStatus: .report)
-        } else if UserPermissionInfo.shared.isReviewInappropriate {
-            self.showRestrictionAlert(permissionStatus: .inappropriate)
-        } else if !(UserPermissionInfo.shared.isReviewed) {
+        } else if UserPermissionInfo.shared.isReviewInappropriate || !(UserPermissionInfo.shared.isReviewed) {
             self.showRestrictionAlert(permissionStatus: .review)
         } else {
             // 아무런 제한이 없을 때 실행되는 action
