7eecf18dd5fe696e0a3a684d4e2a02b89f5fc7e8
eunjuchoi <eunjuzzoo@naver.com>
2022-03-20 02:43:02 +0900

    ✅ Chore: baseURL 분기처리 (#374)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIEssentials/APIConstants.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIEssentials/APIConstants.swift
@@ -10,5 +10,9 @@ import Foundation
 struct APIConstants {
     
     //MARK: Base URL
+    #if DEBUG
+    static let baseURL = "https://asia-northeast3-nadosunbae-server-dev-90ac3.cloudfunctions.net/api"
+    #else
     static let baseURL = "https://asia-northeast3-nadosunbae-server.cloudfunctions.net/api"
+    #endif
 }
