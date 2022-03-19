8092b5b3ec5d77871d0c4e88564839c4d5eb6200
eunjuchoi <eunjuzzoo@naver.com>
2022-03-20 02:46:35 +0900

    ✨ Feat: 글자 수에 따른 UI 분기처리 (#374)

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Review/ReviewMainPostListData.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Review/ReviewMainPostListData.swift
@@ -6,6 +6,10 @@
 //
 
 import Foundation
+import UIKit
+
+let screenSize: CGRect = UIScreen.main.bounds
+let screenWidth = screenSize.width
 
 // MARK: - ReviewMainPostListData
 struct ReviewMainPostListData: Codable {
@@ -40,5 +44,36 @@ struct ReviewWriter: Codable {
         case writerID = "writerId"
         case profileImageID = "profileImageId"
         case nickname, firstMajorName, firstMajorStart, secondMajorName, secondMajorStart
+    }
+}
+
+/// 닉네임 + 전공정보 String으로 반환하는 함수
+func convertToUserInfoString(_ nickname: String, _ firstMajorName: String, _ firstMajorStart: String, _ secondMajorName: String, _ secondMajorStart: String) -> String {
+    if secondMajorName == "미진입" {
+        return nickname + "   " + firstMajorName + " " + firstMajorStart + "  |  " + secondMajorName
+    } else {
+        /// 닉네임 + 본전공명 + 본전공진입시기 + 제2전공 + 제2전공진입시기 글자수 32자 넘을 때 (닉네임 텍스트 크기 고려)
+        if (nickname.count + firstMajorName.count + firstMajorStart.count + secondMajorName.count + secondMajorStart.count) > 30 {
+            
+            if screenWidth == 375 {
+                /// 본전공명 + 본전공진입시기 + 제2전공 + 제2전공진입시기 글자수 35자 넘을 때
+                if (firstMajorName.count + firstMajorStart.count + secondMajorName.count + secondMajorStart.count) > 35 {
+                    return nickname + "\n" + firstMajorName + " " + firstMajorStart + "  |\n" + secondMajorName + " " + secondMajorStart
+                } else {
+                    /// 본전공명 + 본전공진입시기 + 제2전공 + 제2전공진입시기 글자수 35자 넘지 않을 때
+                    return nickname + "\n" + firstMajorName + " " + firstMajorStart + "  |  " + secondMajorName + " " + secondMajorStart
+                }
+            } else {
+                /// 본전공명 + 본전공진입시기 + 제2전공 + 제2전공진입시기 글자수 37자 넘을 때
+                if (firstMajorName.count + firstMajorStart.count + secondMajorName.count + secondMajorStart.count) > 37 {
+                    return nickname + "\n" + firstMajorName + " " + firstMajorStart + "  |\n" + secondMajorName + " " + secondMajorStart
+                } else {
+                    /// 본전공명 + 본전공진입시기 + 제2전공 + 제2전공진입시기 글자수 35자 넘지 않을 때
+                    return nickname + "\n" + firstMajorName + " " + firstMajorStart + "  |  " + secondMajorName + " " + secondMajorStart
+                }
+            }
+        } else {
+            return nickname + "   " + firstMajorName + " " + firstMajorStart + "  |  " + secondMajorName + " " + secondMajorStart
+        }
     }
 }
