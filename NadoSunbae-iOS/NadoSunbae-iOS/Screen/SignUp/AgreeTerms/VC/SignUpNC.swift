320c5fb33dec20d472dfb0622303f3d4242c8089
hwangJi-dev <melon2228@swu.ac.kr>
2022-07-10 23:57:56 +0900

    ➕ Add: SignUpNVC 추가

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/SignUp/AgreeTerms/VC/SignUpNC.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpNC.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/07/10.
+//
+
+import UIKit
+
+class SignUpNC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
