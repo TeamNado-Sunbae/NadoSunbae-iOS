377e0263ec815b053d3e29782c4d840460474a77
hwangJi-dev <melon2228@swu.ac.kr>
2022-07-14 06:38:46 +0900

    ➕ Add: #402 - 나도선배 Style SegmentedControl Class (NadoSegmentedControl) 생성

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/UIComponent/Class/NadoSegmentedControl.swift
@@ -0,0 +1,20 @@
+//
+//  NadoSegmentedControl.swift
+//  NadoSunbae-iOS
+//
+//  Created by hwangJi on 2022/07/14.
+//
+
+import UIKit
+
+class NadoSegmentedControl: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
