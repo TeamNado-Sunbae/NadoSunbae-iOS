77b211ab7be8019c9359098da60121f43633f8a0
hwangJi-dev <melon2228@swu.ac.kr>
2022-07-16 00:27:08 +0900

    ✅ Chore: #407 - BaseQuestionTVC를 상속하여 CommunityTVC 생성

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Community/Cell/TVC/CommunityTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Community/Cell/TVC/CommunityTVC.swift
@@ -6,18 +6,52 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
-class CommunityTVC: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class CommunityTVC: BaseQuestionTVC {
+    
+    // MARK: Properties
+    let categoryLabel = UILabel().then {
+        $0.textColor = .mainDefault
+        $0.font = .PretendardR(size: 12.0)
+        $0.sizeToFit()
     }
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+// MARK: - UI
+extension CommunityTVC {
+    override func configureUI() {
+        super.configureUI()
+        contentView.addSubview(categoryLabel)
+        
+        categoryLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        
+        questionTitleLabel.snp.removeConstraints()
+        questionTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(categoryLabel.snp.bottom).offset(4)
+            $0.leading.trailing.equalTo(categoryLabel)
+        }
+        
+        nicknameLabel.snp.updateConstraints {
+            $0.bottom.equalToSuperview().offset(-23)
+        }
     }
+}
 
+// MARK: - Custom Methods
+extension CommunityTVC {
+    func setCommunityData(data: CommunityPostList) {
+        categoryLabel.text = data.category
+        questionTitleLabel.text = data.title
+        questionContentLabel.text = data.content
+        nicknameLabel.text = data.writer.nickname
+        questionTimeLabel.text = data.createdAt.serverTimeToString(forUse: .forDefault)
+        commentCountLabel.text = "\(data.commentCount)"
+        likeCountLabel.text = "\(data.like.likeCount)"
+        likeImgView.image = data.like.isLiked ? UIImage(named: "heart_filled") : UIImage(named: "btn_heart")
+    }
 }
