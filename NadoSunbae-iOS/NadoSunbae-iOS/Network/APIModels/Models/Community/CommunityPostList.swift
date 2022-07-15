eba331fc56cf55ce3a8a41eb413ca3384682be5d
hwangJi-dev <melon2228@swu.ac.kr>
2022-07-16 00:26:38 +0900

    ✅ Chore: #407 - 임시 CommunityPostList DataModel 생성

--- NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Community/CommunityPostList.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Community/CommunityPostList.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+// MARK: - CommunityPostList
+struct CommunityPostList: Codable {
+    var category: String = ""
+    var postID: Int = 0
+    var title: String = ""
+    var content: String = ""
+    var createdAt: String = ""
+    var writer: Writer = Writer()
+    var like: Like = Like(isLiked: true, likeCount: 0)
+    var commentCount: Int = 0
+
+    enum CodingKeys: String, CodingKey {
+        case category = "category"
+        case postID = "postId"
+        case title = "title"
+        case content = "content"
+        case createdAt = "createdAt"
+        case writer = "writer"
+        case commentCount = "commentCount"
+        case like = "like"
+    }
+    
+    struct Writer: Codable {
+        var writerID: Int = 0
+        var profileImageID: Int = 0
+        var nickname: String = ""
+
+        enum CodingKeys: String, CodingKey {
+            case writerID = "writerId"
+            case profileImageID = "profileImageId"
+            case nickname = "nickname"
+        }
+    }
+}
