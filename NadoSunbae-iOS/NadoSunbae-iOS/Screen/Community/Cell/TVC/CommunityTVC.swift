e02d00a20b1d8b8b73d8accbdb6f2fde4205fdb0
hwangJi-dev <melon2228@swu.ac.kr>
2022-07-16 00:25:46 +0900

    ➕ Add: #407 - CommuintyTVC, CommunityPostList 파일 추가

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Community/Cell/TVC/CommunityTVC.swift
@@ -0,0 +1,23 @@
+//
+//  CommunityTVC.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/07/15.
+//
+
+import UIKit
+
+class CommunityTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
