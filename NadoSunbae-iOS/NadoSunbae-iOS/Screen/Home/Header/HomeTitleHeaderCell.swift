b4a5a060c6a8ee78238b5e3ff51edb0f0019ac86
dev-madilyn <lollin0@naver.com>
2022-08-05 23:10:41 +0900

    ➕ Add: 홈 타이틀 헤더 셀 클래스 생성 (#415)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/Header/HomeTitleHeaderCell.swift
@@ -0,0 +1,47 @@
+//
+//  HomeTitleHeaderCell.swift
+//  NadoSunbae
+//
+//  Created by madilyn on 2022/08/05.
+//
+
+import UIKit
+
+class HomeTitleHeaderCell: UITableViewCell {
+    
+    // MARK: Components
+    private let titleLabel = UILabel().then {
+        $0.font = .PretendardB(size: 18)
+        $0.textColor = .mainBlack
+    }
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTitleLabel(titleText: String) {
+        titleLabel.text = titleText
+        titleLabel.sizeToFit()
+    }
+}
+
+// MARK: - UI
+extension HomeTitleHeaderCell {
+    private func configureUI() {
+        tintColor = .white
+        backgroundColor = .white
+        
+        addSubviews([titleLabel])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.left.equalToSuperview().inset(24)
+        }
+    }
+}
