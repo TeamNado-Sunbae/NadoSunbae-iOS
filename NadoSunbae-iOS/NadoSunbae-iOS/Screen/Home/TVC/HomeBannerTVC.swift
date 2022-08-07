218802368fb7a6f005687b568943f2106d42457b
dev-madilyn <lollin0@naver.com>
2022-08-08 01:38:21 +0900

    ✅ Chore: Banner CV, CVFlowLayout 선언 (#420)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeBannerTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeBannerTVC.swift
@@ -12,7 +12,30 @@ import Then
 class HomeBannerTVC: BaseTVC {
     
     // MARK: Components
-    private let bannerCV = UICollectionView()
+    private let bannerCV = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        $0.showsHorizontalScrollIndicator = false
+        $0.isPagingEnabled = true
+    }
+    private lazy var segmentedControl = UISegmentedControl().then {
+        $0.removeAllSegments()
+        for i in 0..<bannerImaURLsData.count - 2 {
+            $0.insertSegment(with: UIImage(named: "unselectedSegmentImage"), at: i, animated: false)
+        }
+        $0.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        $0.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        $0.selectedSegmentIndex = 0
+        $0.setImage(UIImage(named: "selectedSegmentImage"), forSegmentAt: $0.selectedSegmentIndex)
+        $0.isUserInteractionEnabled = false
+    }
+    
+    // MARK: - Properties
+    private lazy var CVFlowLayout = UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .horizontal
+        $0.minimumLineSpacing = 0
+        $0.minimumInteritemSpacing = 0
+        $0.sectionInset = .zero
+        $0.itemSize = CGSize.init(width: screenWidth, height: 104)
+    }
     
     // MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
