0754f41810e2401d5483f69ea976e80539e89e75
dev-madilyn <lollin0@naver.com>
2022-08-08 01:38:34 +0900

    ✅ Chore: banner에 들어갈 image url 배열 선언 (#420)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeBannerTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeBannerTVC.swift
@@ -37,6 +37,9 @@ class HomeBannerTVC: BaseTVC {
         $0.itemSize = CGSize.init(width: screenWidth, height: 104)
     }
     
+    /// 첫 인덱스 -> 마지막 이미지, 마지막 인덱스 -> 첫 이미지 추가하기!
+    private var bannerImaURLsData = ["https://user-images.githubusercontent.com/43312096/183249441-5ad2cffb-db34-421a-a23c-915415c0593a.png", "https://user-images.githubusercontent.com/43312096/183249428-0c5b531b-6290-40c3-80ec-2517c9c990f8.png", "https://user-images.githubusercontent.com/43312096/183249432-034f6b8b-5518-49ce-bd69-4e6f4764b071.png", "https://user-images.githubusercontent.com/43312096/183249433-17ae6687-1632-423b-929b-6c57ac51edf3.png", "https://user-images.githubusercontent.com/43312096/183249439-97333cf5-a88d-4d72-8dca-9641e4587260.png", "https://user-images.githubusercontent.com/43312096/183249441-5ad2cffb-db34-421a-a23c-915415c0593a.png", "https://user-images.githubusercontent.com/43312096/183249428-0c5b531b-6290-40c3-80ec-2517c9c990f8.png"]
+    
     // MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
