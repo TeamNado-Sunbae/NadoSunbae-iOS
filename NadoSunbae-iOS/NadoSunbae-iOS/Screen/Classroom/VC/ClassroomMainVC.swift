30bbf49b2b81aee6311416f41b88c37ccb0d1f00
eunjuchoi <eunjuzzoo@naver.com>
2022-08-08 13:06:29 +0900

    ✅ Chore: 과방 메인 기본 구조 수정 (#418)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
@@ -19,15 +19,13 @@ class ClassroomMainVC: BaseVC {
         $0.textColor = .black
         $0.sizeToFit()
     }
-    
+
     private let bottomArrowImgView = UIImageView().then {
         $0.image = UIImage(named: "btnArrow")
     }
-    
+
     private let majorSelectBtn = UIButton()
-    private var classroomContainerView = NadoHorizonContainerViews().then {
-        $0.setMultiplier(containerCount: 2)
-    }
+    private let classroomTV = UITableView()
     
     // MARK: IBOutlet
     @IBOutlet var topNaviView: UIView!
@@ -35,10 +33,8 @@ class ClassroomMainVC: BaseVC {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .paleGray
         configureUI()
         tapMajorSelectBtn()
-        configureContainerView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,7 +48,8 @@ class ClassroomMainVC: BaseVC {
 extension ClassroomMainVC {
     private func configureUI() {
         topNaviView.addSubviews([majorLabel, bottomArrowImgView, majorSelectBtn])
-        self.view.addSubview(classroomContainerView)
+        self.view.addSubview(classroomTV)
+        self.view.backgroundColor = .paleGray
         
         majorLabel.snp.makeConstraints {
             $0.bottom.equalTo(topNaviView.snp.bottom).offset(-24)
@@ -70,40 +67,10 @@ extension ClassroomMainVC {
             $0.leading.equalTo(majorLabel)
         }
         
-        classroomContainerView.snp.makeConstraints {
+        classroomTV.snp.makeConstraints {
             $0.top.equalTo(topNaviView.snp.bottom)
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
-    }
-    
-    /// containerView 컨텐츠 VC를 설정하는 메서드
-    func configureContainerView() {
-        let questionSB: UIStoryboard = UIStoryboard(name: Identifiers.QuestionSB, bundle: nil)
-        let infoSB: UIStoryboard = UIStoryboard(name: Identifiers.InfoSB, bundle: nil)
-
-        guard let questionMainVC = questionSB.instantiateViewController(identifier: QuestionMainVC.className) as? QuestionMainVC else { return }
-        guard let infoMainVC = infoSB.instantiateViewController(identifier: InfoMainVC.className) as? InfoMainVC else { return }
-        
-        questionMainVC.sendSegmentStateDelegate = self
-        infoMainVC.sendSegmentStateDelegate = self
-        
-        [questionMainVC, infoMainVC].forEach {
-            addChild($0)
-            $0.view.translatesAutoresizingMaskIntoConstraints = false
-        }
-        
-        classroomContainerView.firstContainerView.addSubview(questionMainVC.view)
-        classroomContainerView.secondContainerView.addSubview(infoMainVC.view)
-        
-        questionMainVC.view.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalTo(self.classroomContainerView)
-        }
-        
-        infoMainVC.view.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalTo(classroomContainerView.secondContainerView)
-        }
-        
-        questionMainVC.didMove(toParent: self)
     }
 }
 
@@ -138,21 +105,6 @@ extension ClassroomMainVC: UIViewControllerTransitioningDelegate {
     /// presentationController - forPresented
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         HalfModalPresentationController(presentedViewController: presented, presenting: presenting)
-    }
-}
-
-// MARK: - SendSegmentStateDelegate
-extension ClassroomMainVC: SendSegmentStateDelegate {
-     
-    /// segment가 클릭되면 index에 따라 ContainerView의 ContentOffset.x 좌표를 바꿔주는 메서드
-    func sendSegmentClicked(index: Int) {
-        if index == 0 {
-            makeScreenAnalyticsEvent(screenName: "ClassRoom_Question Tab", screenClass: QuestionMainVC.className)
-            classroomContainerView.externalSV.contentOffset.x = 0
-        } else {
-            makeScreenAnalyticsEvent(screenName: "ClassRoom_Info Tab", screenClass: InfoMainVC.className)
-            classroomContainerView.externalSV.contentOffset.x += screenWidth
-        }
     }
 }
 
