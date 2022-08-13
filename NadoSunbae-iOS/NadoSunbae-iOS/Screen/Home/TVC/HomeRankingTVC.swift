eea764f3118ffd4ef7d9ec0064bf6dd3166ea024
dev-madilyn <lollin0@naver.com>
2022-08-14 02:18:06 +0900

    ✅ Chore: 랭커 프로퍼티 생성 및 UI 코드 작성 (#428)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRankingTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRankingTVC.swift
@@ -13,6 +13,11 @@ final class HomeRankingTVC: BaseTVC {
     private let backgroundImgView = UIImageView().then {
         $0.image = UIImage(named: "homeRankingBG")
     }
+    private let firstRankerView = HomeRankerView(type: .first)
+    private let secondRankerView = HomeRankerView(type: .secondThird)
+    private let thirdRankerView = HomeRankerView(type: .secondThird)
+    private let fourthRankerView = HomeRankerView(type: .fourthFifth)
+    private let fifthRankerView = HomeRankerView(type: .fourthFifth)
     
     // MARK: Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -29,11 +34,46 @@ final class HomeRankingTVC: BaseTVC {
 // MARK: - UI
 extension HomeRankingTVC {
     private func configureUI() {
-        contentView.addSubviews([backgroundImgView])
+        contentView.addSubviews([backgroundImgView, firstRankerView, secondRankerView, thirdRankerView, fourthRankerView, fifthRankerView])
         
         backgroundImgView.snp.makeConstraints {
             $0.top.left.right.equalToSuperview()
             $0.bottom.equalToSuperview().inset(20)
+        }
+        
+        firstRankerView.snp.makeConstraints {
+            $0.top.equalTo(backgroundImgView.snp.top).inset(24.adjusted)
+            $0.centerX.equalTo(backgroundImgView)
+            $0.width.equalTo(screenWidth * 0.192)
+            $0.height.equalTo(240.adjusted * 0.491666)
+        }
+        
+        secondRankerView.snp.makeConstraints {
+            $0.top.equalTo(backgroundImgView.snp.top).inset(56.adjusted)
+            $0.centerX.equalTo(backgroundImgView.snp.centerX).offset(-80.adjusted)
+            $0.width.equalTo(screenWidth * 0.184)
+            $0.height.equalTo(240.adjusted * 0.441666)
+        }
+        
+        thirdRankerView.snp.makeConstraints {
+            $0.top.equalTo(backgroundImgView.snp.top).inset(56.adjusted)
+            $0.centerX.equalTo(backgroundImgView.snp.centerX).offset(80.adjusted)
+            $0.width.equalTo(screenWidth * 0.184)
+            $0.height.equalTo(240.adjusted * 0.441666)
+        }
+        
+        fourthRankerView.snp.makeConstraints {
+            $0.top.equalTo(backgroundImgView.snp.top).inset(104.adjusted)
+            $0.centerX.equalTo(backgroundImgView.snp.centerX).offset(-151.adjusted)
+            $0.width.equalTo(screenWidth * 0.146666)
+            $0.height.equalTo(240.adjusted * 0.3)
+        }
+        
+        fifthRankerView.snp.makeConstraints {
+            $0.top.equalTo(backgroundImgView.snp.top).inset(104.adjusted)
+            $0.centerX.equalTo(backgroundImgView.snp.centerX).offset(151.adjusted)
+            $0.width.equalTo(screenWidth * 0.146666)
+            $0.height.equalTo(240.adjusted * 0.3)
         }
     }
 }
