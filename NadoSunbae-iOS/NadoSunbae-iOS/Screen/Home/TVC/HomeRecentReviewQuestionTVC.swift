3d858661779191719c58173a10e84d982274be9f
dev-madilyn <lollin0@naver.com>
2022-08-13 19:51:21 +0900

    ➕ Add: 더미데이터 추가 (#426)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRecentReviewQuestionTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRecentReviewQuestionTVC.swift
@@ -23,6 +23,13 @@ class HomeRecentReviewQuestionTVC: BaseTVC {
         $0.sectionInset = .zero
         $0.itemSize = CGSize.init(width: (screenWidth - 32) * 2 / 3, height: 153)
     }
+    var recentReviewsDummyData: HomeRecentReviewResponseModel = [
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "원라인리뷰..~", majorName: "어쩌구공학과", createdAt: "2022-06-12T01:35:59.500Z", tagList: [ReviewTagList(tagName: "어쩌구"), ReviewTagList(tagName: "뭘 배우나요?")], like: Like(isLiked: true, likeCount: 13)),
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대40자자자자자자자자잦자자자자자자자자자자자자자자자자자자자자자자자자자자자자자자잦자", majorName: "전공멀로하쥐", createdAt: "2022-06-12T01:35:59.500Z", tagList: [ReviewTagList(tagName: "어쩌구"), ReviewTagList(tagName: "뭘 배우나요?")], like: Like(isLiked: true, likeCount: 13)),
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "원라인리뷰..~", majorName: "이것은긴전공명엄청길어어어어어어어어어어어어어어어어어", createdAt: "2022-06-12T01:35:59.500Z", tagList: [], like: Like(isLiked: true, likeCount: 13)),
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "원라인리뷰..~", majorName: "어쩌구공학과", createdAt: "2022-06-12T01:35:59.500Z", tagList: [ReviewTagList(tagName: "어쩌구"), ReviewTagList(tagName: "뭘 배우나요?")], like: Like(isLiked: true, likeCount: 13)),
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "원라인리뷰..~", majorName: "어쩌구공학과", createdAt: "2022-06-12T01:35:59.500Z", tagList: [ReviewTagList(tagName: "어쩌구"), ReviewTagList(tagName: "뭘 배우나요?")], like: Like(isLiked: true, likeCount: 13)),
+        HomeRecentReviewResponseModelElement(id: 111, oneLineReview: "원라인리뷰..~", majorName: "어쩌구공학과", createdAt: "2022-06-12T01:35:59.500Z", tagList: [ReviewTagList(tagName: "어쩌구"), ReviewTagList(tagName: "뭘 배우나요?")], like: Like(isLiked: true, likeCount: 13))]
     
     // MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
