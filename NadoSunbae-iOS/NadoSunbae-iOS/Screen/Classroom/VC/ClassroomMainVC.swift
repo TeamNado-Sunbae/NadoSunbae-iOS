ee4a54395b21d5a57e2c88c36dac88820d677cd2
eunjuchoi <eunjuzzoo@naver.com>
2022-08-15 02:41:26 +0900

    ✨ Feat: 과방 메인 디폴트 UI 구성 (#418)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/VC/ClassroomMainVC.swift
@@ -8,9 +8,13 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
+import ReactorKit
+import RxDataSources
 import FirebaseAnalytics
 
-class ClassroomMainVC: BaseVC {
+final class ClassroomMainVC: BaseVC, View {
     
     // MARK: Properties
     private let majorLabel = UILabel().then {
@@ -24,16 +28,33 @@ class ClassroomMainVC: BaseVC {
         $0.image = UIImage(named: "btnArrow")
     }
 
+    private let topNaviView = UIView()
     private let majorSelectBtn = UIButton()
-    private let classroomTV = UITableView()
+    private let reviewTV = UITableView().then {
+        $0.rowHeight = UITableView.automaticDimension
+    }
     
-    // MARK: IBOutlet
-    @IBOutlet var topNaviView: UIView!
+    var disposeBag = DisposeBag()
+    let dataSource: RxTableViewSectionedReloadDataSource<ClassroomMainSection> = RxTableViewSectionedReloadDataSource(configureCell: { _, tableView, indexPath, items -> UITableViewCell in
+        
+        switch items {
+        case .imageCell:
+            let cell = tableView.dequeueReusableCell(withIdentifier: ReviewMainImgTVC.className, for: indexPath)
+            return cell
+        case .reviewPostCell(let reactor):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ReviewMainPostTVC.className, for: indexPath) as? ReviewMainPostTVC else { return UITableViewCell() }
+            cell.reactor = reactor
+            return cell
+        }
+    })
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        addShadowToNaviBar()
+        setUpTV()
+        registerTVC()
         tapMajorSelectBtn()
     }
     
@@ -42,13 +63,32 @@ class ClassroomMainVC: BaseVC {
         showTabbar()
         makeScreenAnalyticsEvent(screenName: "ClassRoom Tab", screenClass: ClassroomMainVC.className)
     }
+    
+    func bind(reactor: ClassroomMainReactor) {
+        reviewTV.rx.setDelegate(self).disposed(by: disposeBag)
+        bindAction(reactor)
+        bindState(reactor)
+    }
+}
+
+// MARK: - Bind Action & State
+extension ClassroomMainVC {
+    private func bindAction(_ reactor: ClassroomMainReactor) {
+        
+    }
+    
+    private func bindState(_ reactor: ClassroomMainReactor) {
+        reactor.state.map{$0.sections}.asObservable()
+            .bind(to: self.reviewTV.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
 }
 
 // MARK: - UI
 extension ClassroomMainVC {
     private func configureUI() {
         topNaviView.addSubviews([majorLabel, bottomArrowImgView, majorSelectBtn])
-        self.view.addSubview(classroomTV)
+        self.view.addSubviews([topNaviView, reviewTV])
         self.view.backgroundColor = .paleGray
         
         majorLabel.snp.makeConstraints {
@@ -67,15 +107,50 @@ extension ClassroomMainVC {
             $0.leading.equalTo(majorLabel)
         }
         
-        classroomTV.snp.makeConstraints {
+        topNaviView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(104)
+        }
+        
+        reviewTV.snp.makeConstraints {
             $0.top.equalTo(topNaviView.snp.bottom)
             $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+    
+    /// NaviBar dropShadow 설정 함수
+    private func addShadowToNaviBar() {
+        topNaviView.layer.shadowColor = UIColor(red: 0.898, green: 0.898, blue: 0.91, alpha: 0.16).cgColor
+        topNaviView.layer.shadowOffset = CGSize(width: 0, height: 9)
+        topNaviView.layer.shadowRadius = 18
+        topNaviView.layer.shadowOpacity = 1
+        topNaviView.layer.masksToBounds = false
     }
 }
 
 // MARK: - Custom Methods
 extension ClassroomMainVC {
+    
+    /// cell 등록 함수
+    private func registerTVC() {
+        ReviewMainImgTVC.register(target: reviewTV)
+        ReviewMainPostTVC.register(target: reviewTV)
+        reviewTV.register(ClassroomMainHeaderView.self, forHeaderFooterViewReuseIdentifier: ClassroomMainHeaderView.className)
+    }
+    
+    /// tableView setting 함수
+    private func setUpTV() {
+        self.reviewTV.separatorStyle = .none
+        self.reviewTV.backgroundColor = .paleGray
+
+        /// TableView 하단 space 설정
+        reviewTV.contentInset.bottom = 24
+
+        /// section header 들어가지 않는 section에 padding 값 없도록
+        if #available(iOS 15.0, *) {
+            UITableView.appearance().sectionHeaderTopPadding = 0.0
+        }
+    }
     
     /// HalfModalView를 present하는 메서드
     private func presentHalfModalView() {
@@ -112,5 +187,25 @@ extension ClassroomMainVC: UIViewControllerTransitioningDelegate {
 extension ClassroomMainVC: SendUpdateModalDelegate {
     func sendUpdate(data: Any) {
         majorLabel.text = data as? String
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ClassroomMainVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 1 {
+            guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: ClassroomMainHeaderView.className) as? ClassroomMainHeaderView else { return UIView() }
+            return headerView
+        }
+        return nil
+    }
+    
+    /// section header 높이 설정
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1 {
+            return 56
+        } else {
+            return 0
+        }
     }
 }
