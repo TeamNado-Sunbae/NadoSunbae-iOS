b546654be749db44f58ab6d1e421404a273ba55e
dev-madilyn <lollin0@naver.com>
2022-08-16 13:22:18 +0900

    ⚰️ Remove: 코드리뷰 반영하여 빈 줄 제거

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRecentReviewQuestionTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/TVC/HomeRecentReviewQuestionTVC.swift
@@ -43,7 +43,6 @@ final class HomeRecentReviewQuestionTVC: BaseTVC {
         setRecentCV()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
