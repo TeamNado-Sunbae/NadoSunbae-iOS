23cdaefd0673ef64fe0061c289465d26ad3ad88c
dev-madilyn <lollin0@naver.com>
2022-08-22 23:53:01 +0900

    ✅ Chore: 최근 1:1 질문 뷰에 statusBarView 적용 (#432)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/VC/HomeRecentPersonalQuestionVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/VC/HomeRecentPersonalQuestionVC.swift
@@ -15,6 +15,7 @@ final class HomeRecentPersonalQuestionVC: BaseVC {
         $0.configureTitleLabel(title: "최근 1:1 질문")
         $0.rightCustomBtn.isHidden = true
     }
+    private let statusBarView = NadoStatusBarView(contentText: "특정 학과 선배에게 1:1 질문을 하고 싶다면 과방탭을 이용하세요.", type: .label)
     
     // MARK: Properties
     
@@ -30,11 +31,16 @@ final class HomeRecentPersonalQuestionVC: BaseVC {
 extension HomeRecentPersonalQuestionVC {
     private func configureUI() {
         view.backgroundColor = .paleGray
-        view.addSubviews([naviView])
+        view.addSubviews([naviView, statusBarView])
         
         naviView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(104)
+        }
+        
+        statusBarView.snp.makeConstraints {
+            $0.top.equalTo(naviView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
         }
     }
 }
