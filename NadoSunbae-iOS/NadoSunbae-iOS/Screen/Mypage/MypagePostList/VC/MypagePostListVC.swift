1f451e6b594c8bf651cc14dc70e9c9c00f8eb2f3
dev-madilyn <lollin0@naver.com>
2022-09-11 14:24:26 +0900

    ⚰️ Remove: 기존 UI 코드 삭제 (#443)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypagePostListVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypagePostListVC.swift
@@ -49,41 +49,11 @@ class MypagePostListVC: BaseVC {
         configureContainerView()
         makeScreenAnalyticsEvent(screenName: "Mypage Tab", screenClass: MypagePostListVC.className)
     }
-}
-
-// MARK: - UI
-extension MypagePostListVC {
-    private func configureContainerView() {
-        guard let mypageQuestionListVC = storyboard?.instantiateViewController(withIdentifier: MypageMyPostListVC.className) as? MypageMyPostListVC else { return }
-        guard let mypageInfoListVC = storyboard?.instantiateViewController(withIdentifier: MypageMyPostListVC.className) as? MypageMyPostListVC else { return }
         
-        mypageQuestionListVC.isPostOrAnswer = isPostOrAnswer
-        mypageInfoListVC.isPostOrAnswer = isPostOrAnswer
-        mypageInfoListVC.postType = .information
-        mypageQuestionListVC.sendSegmentStateDelegate = self
-        mypageInfoListVC.sendSegmentStateDelegate = self
         
-        mypageQuestionListVC.view.frame = self.view.bounds
-        mypageInfoListVC.view.frame = self.view.bounds
-        
-        addChild(mypageQuestionListVC)
-        addChild(mypageInfoListVC)
-        
-        mypageQuestionListVC.view.translatesAutoresizingMaskIntoConstraints = false
-        mypageInfoListVC.view.translatesAutoresizingMaskIntoConstraints = false
-        
-        postListContainerView.firstContainerView.addSubview(mypageQuestionListVC.view)
-        postListContainerView.secondContainerView.addSubview(mypageInfoListVC.view)
-        
-        mypageQuestionListVC.view.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalTo(self.postListContainerView)
         }
         
-        mypageInfoListVC.view.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalTo(postListContainerView.secondContainerView)
         }
-        
-        mypageQuestionListVC.didMove(toParent: self)
     }
 }
 
