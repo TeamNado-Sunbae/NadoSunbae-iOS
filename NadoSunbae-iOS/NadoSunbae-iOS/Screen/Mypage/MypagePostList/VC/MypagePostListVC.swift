1b3c15143190372cfb6a40d92101a40610fe7c8b
dev-madilyn <lollin0@naver.com>
2022-09-11 14:26:02 +0900

    ✅ Chore: UI 코드 작성 (#443)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypagePostListVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Mypage/MypagePostList/VC/MypagePostListVC.swift
@@ -18,6 +18,10 @@ class MypagePostListVC: BaseVC {
         }
     }
     private let postListSegmentControl = NadoSegmentedControl(items: ["1:1 질문", "커뮤니티"])
+    private let postListSV = UIScrollView().then {
+        $0.isScrollEnabled = true
+    }
+    private let contentView = UIView()
     private let postListTV = UITableView().then {
         $0.layer.cornerRadius = 16
         $0.layer.borderWidth = 1
@@ -25,6 +29,7 @@ class MypagePostListVC: BaseVC {
         $0.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         $0.contentInset = .zero
         $0.separatorColor = .gray0
+        $0.isScrollEnabled = false
     }
     
     // MARK: Properties
@@ -47,13 +52,43 @@ class MypagePostListVC: BaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureContainerView()
+        
         makeScreenAnalyticsEvent(screenName: "Mypage Tab", screenClass: MypagePostListVC.className)
     }
+    private func setPostListTV() {
+        postListTV.dataSource = self
+        postListTV.delegate = self
         
+        postListTV.register(EntireQuestionListTVC.self, forCellReuseIdentifier: EntireQuestionListTVC.className)
         
+        postListTV.removeSeparatorsOfEmptyCellsAndLastCell()
+    }
+    
+    private func updateQuestionTVHeight() {
+        postListTV.reloadData()
+        DispatchQueue.main.async {
+            self.postListTV.layoutIfNeeded()
+            self.postListTV.rowHeight = UITableView.automaticDimension
+            self.postListTV.snp.updateConstraints {
+                $0.height.equalTo(self.postListTV.contentSize.height)
+            }
         }
         
         }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MypagePostListVC: UITableViewDelegate {
+    
+    /// estimatedHeightForRowAt
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+
+    /// heightForRowAt
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
 
@@ -65,6 +100,39 @@ extension MypagePostListVC: SendSegmentStateDelegate {
         } else {
             makeScreenAnalyticsEvent(screenName: "Mypage Tab", screenClass: "MypageMyPostAnswerListVC")
             postListContainerView.externalSV.contentOffset.x += screenWidth
+// MARK: - UI
+extension MypagePostListVC {
+    private func configureUI() {
+        view.addSubviews([naviView, postListSegmentControl, postListSV])
+        postListSV.addSubview(contentView)
+        contentView.addSubview(postListTV)
+        
+        naviView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(104)
+        }
+        
+        postListSegmentControl.snp.makeConstraints {
+            $0.top.equalTo(naviView.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.equalTo(160)
+        }
+        
+        postListSV.snp.makeConstraints {
+            $0.top.equalTo(postListSegmentControl.snp.bottom).offset(18)
+            $0.left.right.bottom.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview().priority(.low)
+            $0.centerX.top.bottom.equalToSuperview()
+        }
+        
+        postListTV.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.right.bottom.equalToSuperview().inset(16)
+            $0.height.equalTo(13)
         }
     }
 }
