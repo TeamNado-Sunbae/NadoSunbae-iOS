e4ce3cc46a6eccf176f9f3a458ecac96b7d94044
eunjuchoi <eunjuzzoo@naver.com>
2022-10-03 13:06:47 +0900

    ➕ Add: Public 모델 추가 (#489)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Network/APIModels/Models/Public/ContentListData.swift
@@ -0,0 +1,14 @@
+//
+//  ContentListData.swift
+//  NadoSunbae
+//
+//  Created by EUNJU on 2022/10/03.
+//
+
+import Foundation
+
+// MARK: - ContentList
+struct ContentList: Codable {
+    let title: String
+    let content: String
+}
