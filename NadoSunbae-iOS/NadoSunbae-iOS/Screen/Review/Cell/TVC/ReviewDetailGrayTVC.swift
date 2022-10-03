05ee58388adbcdf66281d8e26459c0e062e32f95
eunjuchoi <eunjuzzoo@naver.com>
2022-10-03 23:16:36 +0900

    ✅ Chore: ReviewDetailGrayTVC UI 수정 (#489)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Cell/TVC/ReviewDetailGrayTVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Review/Cell/TVC/ReviewDetailGrayTVC.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class ReviewDetailGrayTVC: BaseTVC {
 
+    // MARK: IBOutlet
+    @IBOutlet weak var questionStatusLabel: UILabel!
+    
+    // MARK: Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+}
 
+// MARK: - Custom Methods
+extension ReviewDetailGrayTVC {
+    func setData(postData: ReviewPostDetailData) {
+        questionStatusLabel.isHidden = postData.writer.isOnQuestion ? false : true
     }
 }
