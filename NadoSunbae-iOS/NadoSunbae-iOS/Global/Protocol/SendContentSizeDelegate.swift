d90cba347ffbce494615902e1e8a9d2cb942f74a
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-09 00:47:55 +0900

    ➕ Add: contentSize를 전달하는 delegate protocol 생성 (#503)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/SendContentSizeDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendContentSizeDelegate.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/10/07.
+//
+
+import UIKit
+
+protocol SendContentSizeDelegate {
+    func sendContentSize(height: CGFloat)
+}
