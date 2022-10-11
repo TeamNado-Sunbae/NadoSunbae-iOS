c538755821b5b7a914835faab64a2973f347df88
dev-madilyn <lollin0@naver.com>
2022-10-12 01:47:02 +0900

    ✅ Chore: 홈탭 엑세스토큰 갱신 처리 (#509)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/VC/HomeRecentPersonalQuestionVC.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Home/VC/HomeRecentPersonalQuestionVC.swift
@@ -114,6 +114,16 @@ extension HomeRecentPersonalQuestionVC {
                     self.updateQuestionTVHeight()
                     self.updateQuestionTVHeight()
                 }
+            case .requestErr(let res):
+                if let message = res as? String {
+                    debugPrint(message)
+                    self.activityIndicator.stopAnimating()
+                    self.makeAlert(title: "네트워크 오류로 인해\n데이터를 불러올 수 없습니다.\n다시 시도해 주세요.")
+                } else if res is Bool {
+                    self.updateAccessToken { _ in
+                        self.getRecentPersonalQuestionList()
+                    }
+                }
             default:
                 debugPrint(#function, "network error")
             }
