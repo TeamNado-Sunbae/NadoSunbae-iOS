a5d3527b81dd8a1668dee041a0e4293bdf395446
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-15 23:56:47 +0900

    ✅ Chore: QuestionPeopleHeaderView 레이아웃 수정 및 코드 추가 (#524)

--- NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/CVC/QuestionPeopleHeaderView.swift
+++ NadoSunbae-iOS/NadoSunbae-iOS/Screen/Classroom/Cell/CVC/QuestionPeopleHeaderView.swift
@@ -15,4 +15,18 @@ class QuestionPeopleHeaderView: UICollectionReusableView {
             headerTitleLabel.font = .PretendardM(size: 16.0)
         }
     }
+    
+    @IBOutlet weak var headerSubLabel: UILabel!
+    @IBOutlet weak var reviewFilterSwitch: NadoSwitch!
+}
+
+// MARK: - UI
+extension QuestionPeopleHeaderView {
+    
+    /// 질문 가능상태 여부에 따라 UI를 결정짓는 메서드
+    func configureUI(isQuestion: Bool) {
+        headerSubLabel.isHidden = isQuestion ? false : true
+        reviewFilterSwitch.isHidden = isQuestion ? false : true
+    
+    }
 }
