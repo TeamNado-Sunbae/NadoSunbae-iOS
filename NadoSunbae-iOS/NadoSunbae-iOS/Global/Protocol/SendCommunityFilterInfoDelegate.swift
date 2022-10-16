21912a71ec61d24fd9adc65467ac837b925e99a8
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-17 00:10:54 +0900

    ➕ Add: communityFilter의 정보를 전달하는 SendCommunityFilterInfoDelegate 추가 (#541)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/SendCommunityFilterInfoDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendCommunityFilterInfoDelegate.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/10/16.
+//
+
+import Foundation
+
+protocol SendCommunityFilterInfoDelegate {
+    func sendCommunityFilterInfo(majorID: Int, majorName: String)
+}
