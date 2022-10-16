1b0c49041b9ba0c72f5c1efa7aaefa2dc31f4c97
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-17 00:11:25 +0900

    ➕ Add: MajorIDConstants 추가 (#541)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Struct/MajorIDConstants.swift
@@ -0,0 +1,13 @@
+//
+//  MajorIDConstants.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/10/16.
+//
+
+import Foundation
+
+struct MajorIDConstants {
+    static let allMajorID = 0
+    static let regardlessMajorID = 128
+}
