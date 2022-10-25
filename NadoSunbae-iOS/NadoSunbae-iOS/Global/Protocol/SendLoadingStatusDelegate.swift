f3f60d569ab6b5a5a2cf91ec0506a292df9df25f
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-25 20:44:53 +0900

    ➕ Add: SendLoadingStatusDelegate 추가 (#575)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/SendLoadingStatusDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendLoadingStatusDelegate.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/10/25.
+//
+
+import Foundation
+
+protocol SendLoadingStatusDelegate {
+    func sendLoadingStatus(loading: Bool)
+}
