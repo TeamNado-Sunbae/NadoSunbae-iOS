7f7173851b8669173e7c8c4e927d5ceef9ca91f4
hwangJi-dev <melon2228@swu.ac.kr>
2022-11-01 21:16:23 +0900

    ➕ Add: GA 관련 Enum, Delegate 추가 (#673)

---
+++ NadoSunbae-iOS/NadoSunbae-iOS/Global/Protocol/SendGAEventDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendGAEventDelegate.swift
+//  NadoSunbae
+//
+//  Created by hwangJi on 2022/11/01.
+//
+
+import Foundation
+
+protocol SendGAEventDelegate {
+    func sendEvent(eventName: GAEventNameType, parameter: String)
+}
